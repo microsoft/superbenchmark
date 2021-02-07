c744cb5d59cfd783ccef2dd137ddd337d4cd79a0
Guoshuai Zhao <guzhao@microsoft.com>
2021-02-07 15:30:22 +0900

    add unittests back.

---
+++ tests/benchmarks/model_benchmarks/test_model_base.py
@@ -0,0 +1,242 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Tests for BenchmarkRegistry module."""
+
+from superbench.benchmarks import Platform, Framework, Precision, BenchmarkContext, BenchmarkRegistry, BenchmarkType
+from superbench.benchmarks.model_benchmarks import ModelBenchmark
+
+
+class FakeModelBenchmark(ModelBenchmark):
+    """Fake benchmark inherit from ModelBenchmark."""
+    def __init__(self, name, parameters=''):
+        """Constructor.
+
+        Args:
+            name: benchmark name.
+            parameters: benchmark parameters.
+        """
+        super().__init__(name, parameters)
+        self._supported_precision = [Precision.FLOAT32.value, Precision.FLOAT16.value]
+
+    def add_parser_arguments(self):
+        """Add the specified arguments."""
+        super().add_parser_arguments()
+        self._parser.add_argument(
+            '--hidden_size',
+            type=int,
+            default=1024,
+            metavar='',
+            required=False,
+            help='Hidden size',
+        )
+
+        self._parser.add_argument(
+            '--seq_len',
+            type=int,
+            default=512,
+            metavar='',
+            required=False,
+            help='Sequence length',
+        )
+
+    def _init_distributed_setting(self):
+        """Initialize the distributed library and bind the worker to GPU."""
+        pass
+
+    def _generate_dataset(self):
+        """Generate dataset for benchmarking according to shape info."""
+        pass
+
+    def _init_dataloader(self):
+        """Initialize the distributed dataloader."""
+        pass
+
+    def _create_optimizer(self):
+        """Create the optimzier instance used for training."""
+        pass
+
+    def _create_model(self, precision):
+        """Construct the model for benchmarking."""
+        pass
+
+    def _train_step(self, precision):
+        """Define the training process.
+
+        Args:
+            precision (str): precision of model and input data,
+              such as float, half.
+
+        Return:
+            The step-time list of every training step.
+        """
+        duration = []
+        for i in range(self._args.num_steps):
+            duration.append(2)
+        return duration
+
+    def _inference_step(self, precision):
+        """Define the inference process.
+
+        Args:
+            precision (str): precision of model and input data,
+              such as float, half.
+
+        Return:
+            The latency list of every inference operation.
+        """
+        duration = []
+        for i in range(self._args.num_steps):
+            duration.append(4)
+        return duration
+
+    def _cal_params_size(self):
+        """Calculate the parameters scale of the model.
+
+        Return:
+            The count of trainable parameters.
+        """
+        return 200
+
+
+def create_benchmark():
+    """Register and create benchmark."""
+    # Register the FakeModelBenchmark benchmark.
+    BenchmarkRegistry.register_benchmark(
+        'pytorch-fake-model',
+        FakeModelBenchmark,
+        parameters='--hidden_size=2',
+        platform=Platform.CUDA,
+    )
+    context = BenchmarkContext('fake-model', Platform.CUDA, parameters='--num_steps=8', framework=Framework.PYTORCH)
+    name = BenchmarkRegistry._BenchmarkRegistry__get_benchmark_name(context)
+    assert (name)
+    (benchmark_class, predefine_params) = BenchmarkRegistry._BenchmarkRegistry__select_benchmark(name, context.platform)
+    assert (benchmark_class)
+    BenchmarkRegistry.clean_benchmarks()
+    return (benchmark_class, name, predefine_params + ' ' + context.parameters)
+
+
+def test_arguments_related_interfaces():
+    """Test arguments related interfaces.
+
+    Benchmark.add_parser_arguments(),
+    Benchmark.parse_args(),
+    Benchmark.get_configurable_settings()
+    """
+    (benchmark_class, name, parameters) = create_benchmark()
+
+    benchmark = benchmark_class(name, parameters)
+    benchmark.add_parser_arguments()
+    benchmark.parse_args()
+
+    settings = benchmark.get_configurable_settings()
+    expected_settings = """optional arguments:
+  --run_count           The run count of benchmark.
+  --duration            The elapsed time of benchmark.
+  --num_warmup          The number of warmup step
+  --num_steps           The number of test step
+  --batch_size          The number of batch size
+  --precision  [ ...]   Model precision. E.g. float16 float32 float64 bfloat16
+                        uint8 int8 int16 int32 int64.
+  --model_action  [ ...]
+                        Benchmark type. E.g. train inference.
+  --distributed_mode    Distributed mode. E.g. pytorch-ddp horovod
+  --hidden_size         Hidden size
+  --seq_len             Sequence length"""
+    assert (settings == expected_settings)
+
+
+def test_preprocess():
+    """Test interface Benchmark._preprocess()."""
+    (benchmark_class, name, parameters) = create_benchmark()
+
+    benchmark = benchmark_class(name, parameters)
+    benchmark._preprocess()
+    assert (benchmark._result._BenchmarkResult__type == BenchmarkType.MODEL.value)
+
+    settings = benchmark.get_configurable_settings()
+    expected_settings = """optional arguments:
+  --run_count           The run count of benchmark.
+  --duration            The elapsed time of benchmark.
+  --num_warmup          The number of warmup step
+  --num_steps           The number of test step
+  --batch_size          The number of batch size
+  --precision  [ ...]   Model precision. E.g. float16 float32 float64 bfloat16
+                        uint8 int8 int16 int32 int64.
+  --model_action  [ ...]
+                        Benchmark type. E.g. train inference.
+  --distributed_mode    Distributed mode. E.g. pytorch-ddp horovod
+  --hidden_size         Hidden size
+  --seq_len             Sequence length"""
+    assert (settings == expected_settings)
+
+
+def test_train():
+    """Test interface Benchmark.__train()."""
+    (benchmark_class, name, parameters) = create_benchmark()
+
+    benchmark = benchmark_class(name, parameters)
+    benchmark._preprocess()
+    benchmark._ModelBenchmark__train('float32')
+    expected_result = (
+        '{"name": "pytorch-fake-model", "type": "model", "run_count": 1, "return_code": 0, '
+        '"start_time": null, "end_time": null, "raw_data": {"steptime_train_float32": [[2, 2, 2, 2, 2, 2, 2, 2]], '
+        '"throughput_train_float32": [[16000.0, 16000.0, 16000.0, 16000.0, 16000.0, 16000.0, 16000.0, 16000.0]]}, '
+        '"result": {"steptime_train_float32": [2.0], "throughput_train_float32": [16000.0]}}'
+    )
+    assert (benchmark._result.to_string() == expected_result)
+
+
+def test_inference():
+    """Test interface Benchmark.__inference()."""
+    (benchmark_class, name, parameters) = create_benchmark()
+
+    benchmark = benchmark_class(name, parameters)
+    benchmark._preprocess()
+    benchmark._ModelBenchmark__inference('float16')
+    expected_result = (
+        '{"name": "pytorch-fake-model", "type": "model", "run_count": 1, "return_code": 0, '
+        '"start_time": null, "end_time": null, "raw_data": {"steptime_inference_float16": [[4, 4, 4, 4, 4, 4, 4, 4]], '
+        '"throughput_inference_float16": [[8000.0, 8000.0, 8000.0, 8000.0, 8000.0, 8000.0, 8000.0, 8000.0]]}, '
+        '"result": {"steptime_inference_float16": [4.0], "throughput_inference_float16": [8000.0]}}'
+    )
+    assert (benchmark._result.to_string() == expected_result)
+
+
+def test_benchmark():
+    """Test interface Benchmark._benchmark()."""
+    (benchmark_class, name, parameters) = create_benchmark()
+
+    benchmark = benchmark_class(name, parameters)
+    benchmark._preprocess()
+    benchmark._benchmark()
+    assert (benchmark.name == 'pytorch-fake-model')
+    assert (benchmark.type == BenchmarkType.MODEL.value)
+    assert (benchmark.run_count == 1)
+    assert (benchmark.return_code == 0)
+    expected_raw_data = {
+        'steptime_train_float32': [[2, 2, 2, 2, 2, 2, 2, 2]],
+        'throughput_train_float32': [[16000.0, 16000.0, 16000.0, 16000.0, 16000.0, 16000.0, 16000.0, 16000.0]],
+        'steptime_train_float16': [[2, 2, 2, 2, 2, 2, 2, 2]],
+        'throughput_train_float16': [[16000.0, 16000.0, 16000.0, 16000.0, 16000.0, 16000.0, 16000.0, 16000.0]]
+    }
+    assert (benchmark.raw_data == expected_raw_data)
+    expected_result = {
+        'steptime_train_float32': [2.0],
+        'throughput_train_float32': [16000.0],
+        'steptime_train_float16': [2.0],
+        'throughput_train_float16': [16000.0]
+    }
+    assert (benchmark.result == expected_result)
+
+    expected_serialized_result = (
+        '{"name": "pytorch-fake-model", "type": "model", "run_count": 1, "return_code": 0, "start_time": null, '
+        '"end_time": null, "raw_data": {"steptime_train_float32": [[2, 2, 2, 2, 2, 2, 2, 2]], '
+        '"throughput_train_float32": [[16000.0, 16000.0, 16000.0, 16000.0, 16000.0, 16000.0, 16000.0, 16000.0]], '
+        '"steptime_train_float16": [[2, 2, 2, 2, 2, 2, 2, 2]], '
+        '"throughput_train_float16": [[16000.0, 16000.0, 16000.0, 16000.0, 16000.0, 16000.0, 16000.0, 16000.0]]}, '
+        '"result": {"steptime_train_float32": [2.0], "throughput_train_float32": [16000.0], '
+        '"steptime_train_float16": [2.0], "throughput_train_float16": [16000.0]}}'
+    )
+    assert (benchmark.serialized_result == expected_serialized_result)
---
+++ tests/benchmarks/test_registry.py
@@ -0,0 +1,209 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""Tests for BenchmarkRegistry module."""
+
+import re
+
+from superbench.benchmarks import Platform, Framework, BenchmarkType, BenchmarkContext, BenchmarkRegistry
+from superbench.benchmarks.micro_benchmarks import MicroBenchmark
+
+
+class AccumulationBenchmark(MicroBenchmark):
+    """Benchmark that do accumulation from lower_bound to upper_bound."""
+    def __init__(self, name, parameters=''):
+        """Constructor.
+
+        Args:
+            name: benchmark name.
+            parameters: benchmark parameters.
+        """
+        super().__init__(name, parameters)
+
+    def add_parser_arguments(self):
+        """Add the specified arguments."""
+        super().add_parser_arguments()
+
+        self._parser.add_argument(
+            '--lower_bound',
+            type=int,
+            default=0,
+            metavar='',
+            required=False,
+            help='The lower bound for accumulation.',
+        )
+
+        self._parser.add_argument(
+            '--upper_bound',
+            type=int,
+            default=2,
+            metavar='',
+            required=False,
+            help='The upper bound for accumulation.',
+        )
+
+    def _benchmark(self):
+        """Implementation for benchmarking."""
+        raw_data = []
+        result = 0
+        for i in range(self._args.lower_bound, self._args.upper_bound):
+            result += i
+            raw_data.append(result)
+
+        metric = 'accumulation_result'
+        self._result.add_raw_data(metric, raw_data)
+        self._result.add_result(metric, result)
+
+
+def test_register_benchmark():
+    """Test interface BenchmarkRegistry.register_benchmark()."""
+    # Register the benchmark for all platform if use default platform.
+    BenchmarkRegistry.register_benchmark('accumulation', AccumulationBenchmark)
+    for platform in Platform:
+        context = BenchmarkContext('accumulation', platform)
+        assert (BenchmarkRegistry.is_benchmark_registered(context))
+
+    BenchmarkRegistry.clean_benchmarks()
+
+    # Register the benchmark for CUDA platform if use platform=Platform.CUDA.
+    BenchmarkRegistry.register_benchmark('accumulation-cuda', AccumulationBenchmark, platform=Platform.CUDA)
+    context = BenchmarkContext('accumulation-cuda', Platform.CUDA)
+    assert (BenchmarkRegistry.is_benchmark_registered(context))
+    context = BenchmarkContext('accumulation-cuda', Platform.ROCM)
+    assert (BenchmarkRegistry.is_benchmark_registered(context) is False)
+
+    BenchmarkRegistry.clean_benchmarks()
+
+
+def test_is_benchmark_context_valid():
+    """Test interface BenchmarkRegistry.is_benchmark_context_valid()."""
+    # Positive case.
+    context = BenchmarkContext('accumulation', Platform.CPU)
+    assert (BenchmarkRegistry.is_benchmark_context_valid(context))
+
+    # Negative case.
+    context = 'context'
+    assert (BenchmarkRegistry.is_benchmark_context_valid(context) is False)
+    context = None
+    assert (BenchmarkRegistry.is_benchmark_context_valid(context) is False)
+
+
+def test_get_benchmark_name():
+    """Test interface BenchmarkRegistry.get_benchmark_name()."""
+    # Register benchmarks for testing.
+    benchmark_names = ['accumulation', 'pytorch-accumulation', 'tf1-accumulation', 'onnx-accumulation']
+    for name in benchmark_names:
+        BenchmarkRegistry.register_benchmark(name, AccumulationBenchmark)
+
+    # Test benchmark name for different Frameworks.
+    benchmark_frameworks = [Framework.NONE, Framework.PYTORCH, Framework.TENSORFLOW, Framework.ONNX]
+    for i in range(len(benchmark_names)):
+        context = BenchmarkContext('accumulation', Platform.CPU, framework=benchmark_frameworks[i])
+        name = BenchmarkRegistry._BenchmarkRegistry__get_benchmark_name(context)
+        assert (name == benchmark_names[i])
+
+    BenchmarkRegistry.clean_benchmarks()
+
+
+def test_check_parameters():
+    """Test interface BenchmarkRegistry.check_parameters()."""
+    # Register benchmarks for testing.
+    BenchmarkRegistry.register_benchmark('accumulation', AccumulationBenchmark)
+
+    # Positive case.
+    context = BenchmarkContext('accumulation', Platform.CPU, parameters='--lower_bound=1')
+    assert (BenchmarkRegistry.check_parameters(context))
+
+    # Negative  case.
+    context = BenchmarkContext('accumulation', Platform.CPU, parameters='--lower=1')
+    assert (BenchmarkRegistry.check_parameters(context) is False)
+
+    BenchmarkRegistry.clean_benchmarks()
+
+
+def test_get_benchmark_configurable_settings():
+    """Test BenchmarkRegistry interface.
+
+    BenchmarkRegistry.get_benchmark_configurable_settings().
+    """
+    # Register benchmarks for testing.
+    BenchmarkRegistry.register_benchmark('accumulation', AccumulationBenchmark)
+
+    context = BenchmarkContext('accumulation', Platform.CPU)
+    settings = BenchmarkRegistry.get_benchmark_configurable_settings(context)
+
+    expected = """optional arguments:
+  --run_count     The run count of benchmark.
+  --duration      The elapsed time of benchmark.
+  --lower_bound   The lower bound for accumulation.
+  --upper_bound   The upper bound for accumulation."""
+
+    assert (settings == expected)
+
+    BenchmarkRegistry.clean_benchmarks()
+
+
+def test_launch_benchmark():
+    """Test interface BenchmarkRegistry.launch_benchmark()."""
+    # Register benchmarks for testing.
+    BenchmarkRegistry.register_benchmark(
+        'accumulation', AccumulationBenchmark, parameters='--upper_bound=5', platform=Platform.CPU
+    )
+    BenchmarkRegistry.register_benchmark(
+        'tf1-accumulation', AccumulationBenchmark, parameters='--upper_bound=5', platform=Platform.CPU
+    )
+
+    # Launch benchmark.
+    context = BenchmarkContext(
+        'accumulation', Platform.CPU, parameters='--lower_bound=1', framework=Framework.TENSORFLOW
+    )
+
+    if BenchmarkRegistry.check_parameters(context):
+        benchmark = BenchmarkRegistry.launch_benchmark(context)
+        assert (benchmark)
+        assert (benchmark.name == 'tf1-accumulation')
+        assert (benchmark.type == BenchmarkType.MICRO.value)
+        assert (benchmark.run_count == 1)
+        assert (benchmark.return_code == 0)
+        assert (benchmark.raw_data == {'accumulation_result': [[1, 3, 6, 10]]})
+        assert (benchmark.result == {'accumulation_result': [10]})
+
+        # replace the timestamp as "0"
+        result = re.sub(r'\"\d+-\d+-\d+ \d+:\d+:\d+\"', '\"0\"', benchmark.serialized_result)
+        expected = (
+            '{"name": "tf1-accumulation", "type": "micro", "run_count": 1, '
+            '"return_code": 0, "start_time": "0", "end_time": "0", '
+            '"raw_data": {"accumulation_result": [[1, 3, 6, 10]]}, '
+            '"result": {"accumulation_result": [10]}}'
+        )
+        assert (result == expected)
+
+    # Launch benchmark with overrided parameters
+    context = BenchmarkContext('accumulation', Platform.CPU, parameters='--lower_bound=1 --upper_bound=4')
+    if BenchmarkRegistry.check_parameters(context):
+        benchmark = BenchmarkRegistry.launch_benchmark(context)
+        assert (benchmark)
+        assert (benchmark.name == 'accumulation')
+        assert (benchmark.type == BenchmarkType.MICRO.value)
+        assert (benchmark.run_count == 1)
+        assert (benchmark.return_code == 0)
+        assert (benchmark.raw_data == {'accumulation_result': [[1, 3, 6]]})
+        assert (benchmark.result == {'accumulation_result': [6]})
+
+        # replace the timestamp as "0"
+        result = re.sub(r'\"\d+-\d+-\d+ \d+:\d+:\d+\"', '\"0\"', benchmark.serialized_result)
+        expected = (
+            '{"name": "accumulation", "type": "micro", "run_count": 1, '
+            '"return_code": 0, "start_time": "0", "end_time": "0", '
+            '"raw_data": {"accumulation_result": [[1, 3, 6]]}, '
+            '"result": {"accumulation_result": [6]}}'
+        )
+        assert (result == expected)
+
+    # Failed to launch benchmark.
+    context = BenchmarkContext(
+        'accumulation', Platform.CPU, parameters='--lower_bound=1 --upper_bound=4', framework=Framework.PYTORCH
+    )
+    assert (BenchmarkRegistry.check_parameters(context) is False)
+
+    BenchmarkRegistry.clean_benchmarks()
