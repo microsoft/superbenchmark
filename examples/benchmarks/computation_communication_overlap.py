9f0a52bd2d0915dca9bdbfc2d96eccbd8b54a588
v-yujiang <v-yujiang@microsoft.com>
2021-04-09 12:50:15 +0000

    Benchmarks: Add Benchmark - add computation and communication overlap micro benchmark

---
+++ examples/benchmarks/computation_communication_overlap.py
@@ -0,0 +1,36 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+"""Micro benchmark example for computation and communication overlap with pytorch.
+
+Commands to run:
+  python3 -m torch.distributed.launch --nproc_per_node=8 examples/benchmarks/computation_communication_overlap.py
+"""
+
+from superbench.benchmarks import Framework, BenchmarkRegistry
+from superbench.common.utils import logger
+
+if __name__ == '__main__':
+    context = BenchmarkRegistry.create_benchmark_context(
+        'computation-communication-overlap-mul', parameters='--num_steps 2000', framework=Framework.PYTORCH
+    )
+
+    benchmark = BenchmarkRegistry.launch_benchmark(context)
+    if benchmark:
+        logger.info(
+            'benchmark: {}, return code: {}, result: {}'.format(
+                benchmark.name, benchmark.return_code, benchmark.result
+            )
+        )
+
+    context = BenchmarkRegistry.create_benchmark_context(
+        'computation-communication-overlap-matmul', parameters='--num_steps 2000', framework=Framework.PYTORCH
+    )
+
+    benchmark = BenchmarkRegistry.launch_benchmark(context)
+    if benchmark:
+        logger.info(
+            'benchmark: {}, return code: {}, result: {}'.format(
+                benchmark.name, benchmark.return_code, benchmark.result
+            )
+        )
--- examples/benchmarks/sharding_matmul.py
+++ examples/benchmarks/sharding_matmul.py
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-"""Model benchmark example for sharding-matmul with pytorch.
+"""Micro benchmark example for sharding-matmul with pytorch.
 
 Commands to run:
   python3 -m torch.distributed.launch --nproc_per_node=8 examples/benchmarks/sharding_matmul.py
