ce3ed24ab7744db6f1e1018e2d19fed81575fd1c
guoshzhao <guzhao@microsoft.com>
2021-04-16 13:17:42 +0800

    Benchmarks: Code Revision - Fix some issue for BERT benchmark. (#58)
    Benchmarks: Code Revision - Fix some issue for BERT benchmark. (#58)

--- examples/benchmarks/pytorch_bert_large.py
+++ examples/benchmarks/pytorch_bert_large.py
@@ -1,22 +1,35 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-"""Model benchmark example for bert-large.
+"""Model benchmark example for bert-large (24-layer, 1024-hidden, 16-heads, 340M parameters).
 
 Commands to run:
   python3 examples/benchmarks/pytorch_bert_large.py (Single GPU)
-  python3 -m torch.distributed.launch --nproc_per_node=8 examples/benchmarks/pytorch_bert_large.py (Distributed)
+  python3 -m torch.distributed.launch --use_env --nproc_per_node=8 examples/benchmarks/pytorch_bert_large.py \
+      --distributed (Distributed)
 """
 
-from superbench.benchmarks import Framework, BenchmarkRegistry
+import argparse
+
+from superbench.benchmarks import Platform, Framework, BenchmarkRegistry
 from superbench.common.utils import logger
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--distributed', action='store_true', default=False, help='Whether to enable distributed training.'
+    )
+    args = parser.parse_args()
+
+    # Specify the model name and benchmark parameters.
+    model_name = 'bert-large'
+    parameters = '--batch_size 1 --duration 120 --seq_len 128 --precision float32 --run_count 2'
+    if args.distributed:
+        parameters += ' --distributed_impl ddp --distributed_backend nccl'
+
     # Create context for bert-large benchmark and run it for 120 * 2 seconds.
     context = BenchmarkRegistry.create_benchmark_context(
-        'bert-large',
-        parameters='--batch_size 1 --duration 120 --seq_len 8 --precision float32 --run_count 2',
-        framework=Framework.PYTORCH
+        model_name, platform=Platform.CUDA, parameters=parameters, framework=Framework.PYTORCH
     )
 
     benchmark = BenchmarkRegistry.launch_benchmark(context)
