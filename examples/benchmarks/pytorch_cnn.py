65292ae55b153e47f22c7cd7a3d723563c84cc8b
guoshzhao <guzhao@microsoft.com>
2021-04-26 21:23:11 +0800

    Benchmarks: Code Revision - Revise the settings of CNN example models. (#65)
    * revise example settings of cnn models.

--- examples/benchmarks/pytorch_cnn.py
+++ examples/benchmarks/pytorch_cnn.py
@@ -1,11 +1,11 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-"""Model benchmark example for resnet101.
+"""Model benchmark example for CNN models.
 
 Commands to run:
-  python3 examples/benchmarks/pytorch_cnn_resnet101.py (Single GPU)
-  python3 -m torch.distributed.launch --use_env --nproc_per_node=8 examples/benchmarks/pytorch_cnn_resnet101.py \
+  python3 examples/benchmarks/pytorch_cnn.py (Single GPU)
+  python3 -m torch.distributed.launch --use_env --nproc_per_node=8 examples/benchmarks/pytorch_cnn.py \
       --distributed (Distributed)
 """
 
@@ -22,12 +22,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Specify the model name and benchmark parameters.
+    # For example, resnet50, resnet101, resnet152, densenet169, densenet201, vgg11, vgg13, vgg16, vgg19.
     model_name = 'resnet101'
-    parameters = '--batch_size 1 --image_size 224 --precision float32 --num_warmup 8 --num_steps 64 --run_count 2'
+    parameters = '--batch_size 32 --precision float32 float16 --num_warmup 64 --num_steps 2048 --sample_count 8192'
     if args.distributed:
         parameters += ' --distributed_impl ddp --distributed_backend nccl'
 
-    # Create context for resnet101 benchmark and run it for 64 steps.
+    # Create context for resnet101 benchmark and run it for 2048 steps.
     context = BenchmarkRegistry.create_benchmark_context(
         model_name, platform=Platform.CUDA, parameters=parameters, framework=Framework.PYTORCH
     )
