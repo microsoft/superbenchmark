57ce473a024c3fb84f6f38fd0be139f2438e3f4a
Yifan Xiong <yifan.xiong@microsoft.com>
2021-05-11 10:49:22 +0800

    Utils - Support lazy import (#67)
    __Major Revision__
    
    * Support lazy import.
    * Not importing benchmarks when running `help`, `version`, `deploy` commands, etc.

--- superbench/benchmarks/__init__.py
+++ superbench/benchmarks/__init__.py
@@ -3,10 +3,22 @@
 
 """Exposes interfaces of benchmarks used by SuperBench executor."""
 
+import importlib
+
 from superbench.benchmarks.return_code import ReturnCode
 from superbench.benchmarks.context import Platform, Framework, Precision, ModelAction, BenchmarkType, BenchmarkContext
-from superbench.benchmarks.registry import BenchmarkRegistry
-from superbench.benchmarks import model_benchmarks, micro_benchmarks, docker_benchmarks    # noqa pylint: disable=unused-import
+from superbench.common.utils import LazyImport
+
+BenchmarkRegistry = LazyImport(
+    'superbench.benchmarks.registry', 'BenchmarkRegistry', lambda: list(
+        map(
+            importlib.import_module, [
+                'superbench.benchmarks.{}'.format(module)
+                for module in ['model_benchmarks', 'micro_benchmarks', 'docker_benchmarks']
+            ]
+        )
+    )
+)
 
 __all__ = [
     'ReturnCode', 'Platform', 'Framework', 'BenchmarkType', 'Precision', 'ModelAction', 'BenchmarkContext',
--- superbench/common/utils/__init__.py
+++ superbench/common/utils/__init__.py
@@ -5,5 +5,6 @@
 
 from superbench.common.utils.logging import SuperBenchLogger, logger
 from superbench.common.utils.file_handler import create_output_dir, get_sb_config
+from superbench.common.utils.lazy_import import LazyImport
 
-__all__ = ['SuperBenchLogger', 'logger', 'create_output_dir', 'get_sb_config']
+__all__ = ['SuperBenchLogger', 'logger', 'create_output_dir', 'get_sb_config', 'LazyImport']
