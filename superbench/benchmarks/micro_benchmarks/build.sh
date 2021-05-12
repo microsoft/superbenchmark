e7845952bbde335c8037887d71635c28df0701e9
Guoshuai Zhao <guzhao@microsoft.com>
2021-05-12 17:12:32 +0900

    add script to build all native benchmarks with cmake.

---
+++ superbench/benchmarks/micro_benchmarks/build.sh
@@ -0,0 +1,17 @@
+# Copyright (c) Microsoft Corporation - All rights reserved
+# Licensed under the MIT License
+
+ #!/bin/bash
+
+SB_PATH="${SB_PATH:-/usr/local/bin}"
+SB_LIB="${SB_LIB:-/usr/local/lib}"
+
+for dir in */ ; do
+    if [ -f $dir/CMakeLists.txt ]; then
+        SOURCE_DIR=$dir
+        BUILD_ROOT=$dir/build
+        mkdir -p $BUILD_ROOT
+        cmake -DCMAKE_INSTALL_PREFIX=$SB_PATH -DCMAKE_BUILD_TYPE=Release -S $SOURCE_DIR -B $BUILD_ROOT
+        cmake --build $BUILD_ROOT --target install
+    fi
+done
