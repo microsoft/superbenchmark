4dc145137120423d3b54e9cf3ad0ff44a9a5abf7
Guoshuai Zhao <guzhao@microsoft.com>
2021-05-13 19:44:04 +0900

    mv build.sh under benchmarks folder.

--- superbench/benchmarks/build.sh
+++ superbench/benchmarks/build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-
 # Copyright (c) Microsoft Corporation - All rights reserved
 # Licensed under the MIT License
 
 
-SB_PATH="${SB_PATH:-/usr/local/bin}"
-SB_LIB="${SB_LIB:-/usr/local/lib}"
+SB_MICRO_PATH="${SB_MICRO_PATH:-/usr/local/bin}"
+SB_MICRO_LIB="${SB_MICRO_LIB:-/usr/local/lib}"
 
-for dir in */ ; do
+for dir in micro_benchmarks/*/ ; do
     if [ -f $dir/CMakeLists.txt ]; then
         SOURCE_DIR=$dir
         BUILD_ROOT=$dir/build
