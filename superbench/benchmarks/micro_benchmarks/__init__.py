d4a328efc160d0949d1cfd696b03158f94afe3bf
root <root@sb-validation-000001.51z1chmys5fuzfqyo4niepozre.bx.internal.cloudapp.net>
2021-05-18 13:31:32 +0000

    add python benchmark for cublas functions, example and test file

--- superbench/benchmarks/micro_benchmarks/__init__.py
+++ superbench/benchmarks/micro_benchmarks/__init__.py
@@ -6,5 +6,8 @@
 from superbench.benchmarks.micro_benchmarks.micro_base import MicroBenchmark, MicroBenchmarkWithInvoke
 from superbench.benchmarks.micro_benchmarks.sharding_matmul import ShardingMatmul
 from superbench.benchmarks.micro_benchmarks.computation_communication_overlap import ComputationCommunicationOverlap
+from superbench.benchmarks.micro_benchmarks.cublas_functions import CublasFunction
 
-__all__ = ['MicroBenchmark', 'MicroBenchmarkWithInvoke', 'ShardingMatmul', 'ComputationCommunicationOverlap']
+__all__ = [
+    'MicroBenchmark', 'MicroBenchmarkWithInvoke', 'ShardingMatmul', 'ComputationCommunicationOverlap', 'CublasFunction'
+]
