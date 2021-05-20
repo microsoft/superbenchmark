07390538375231e67ec34bd962ebde45f90b1930
root <root@sb-validation-000001.51z1chmys5fuzfqyo4niepozre.bx.internal.cloudapp.net>
2021-05-20 05:56:49 +0000

    delete

--- superbench/benchmarks/micro_benchmarks/__init__.py
+++ superbench/benchmarks/micro_benchmarks/__init__.py
@@ -1,8 +1,10 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+"""A module containing all the micro-benchmarks."""
+
 from superbench.benchmarks.micro_benchmarks.micro_base import MicroBenchmark, MicroBenchmarkWithInvoke
 from superbench.benchmarks.micro_benchmarks.sharding_matmul import ShardingMatmul
 from superbench.benchmarks.micro_benchmarks.computation_communication_overlap import ComputationCommunicationOverlap
-from superbench.benchmarks.micro_benchmarks.cublas_functions import CublasFunction
 
-__all__ = [
-    'MicroBenchmark', 'MicroBenchmarkWithInvoke', 'ShardingMatmul', 'ComputationCommunicationOverlap', 'CublasFunction'
-]
+__all__ = ['MicroBenchmark', 'MicroBenchmarkWithInvoke', 'ShardingMatmul', 'ComputationCommunicationOverlap']
