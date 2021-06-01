00bc942a7e12ed43ca23683bdf14bf21046b72d7
yukirora <v-yujiang@microsoft.com>
2021-06-01 05:23:51 +0000

    delete useless return code setting

--- superbench/benchmarks/micro_benchmarks/cublas_function.py
+++ superbench/benchmarks/micro_benchmarks/cublas_function.py
@@ -293,7 +293,6 @@ class CublasBenchmark(MicroBenchmarkWithInvoke):
                     self._curr_run_index, cmd_idx, self._name, raw_output, str(e)
                 )
             )
-            self._result.set_return_code(ReturnCode.MICROBENCHMARK_RESULT_PARSING_FAILURE)
             return False
         if error:
             logger.error(
@@ -301,7 +300,6 @@ class CublasBenchmark(MicroBenchmarkWithInvoke):
                     self._curr_run_index, cmd_idx, self._name, raw_output
                 )
             )
-            self._result.set_return_code(ReturnCode.MICROBENCHMARK_EXECUTION_FAILURE)
             return False
         return True
 
