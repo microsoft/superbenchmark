f4e97599da52cb4816938519a4fb399b50908524
yukirora <v-yujiang@microsoft.com>
2021-07-07 14:01:50 +0800

    revise test to mock test

--- tests/benchmarks/micro_benchmarks/test_memory_copy_bw_performance.py
+++ tests/benchmarks/micro_benchmarks/test_memory_copy_bw_performance.py
@@ -4,33 +4,99 @@
 """Tests for mem-copy-bw benchmark."""
 
 import numbers
+from pathlib import Path
+import os
+import unittest
 
-from tests.helper import decorator
 from superbench.benchmarks import BenchmarkRegistry, BenchmarkType, ReturnCode, Platform
 
 
-@decorator.cuda_test
-def test_memory_copy_bw_performance():
-    """Test mem-copy-bw benchmark."""
-    context = BenchmarkRegistry.create_benchmark_context('mem-copy-bw', Platform.CUDA)
+class MemBwTest(unittest.TestCase):
+    """Tests for MemBw benchmark."""
+    def setUp(self):
+        """Method called to prepare the test fixture."""
+        # Create fake binary file just for testing.
+        os.environ['SB_MICRO_PATH'] = '/tmp/superbench/'
+        binary_path = os.path.join(os.getenv('SB_MICRO_PATH'), 'bin')
+        Path(binary_path).mkdir(parents=True, exist_ok=True)
+        self.__binary_file = Path(os.path.join(binary_path, 'bandwidthTest'))
+        self.__binary_file.touch(mode=0o755, exist_ok=True)
 
-    assert (BenchmarkRegistry.is_benchmark_context_valid(context))
+    def tearDown(self):
+        """Method called after the test method has been called and the result recorded."""
+        self.__binary_file.unlink()
 
-    benchmark = BenchmarkRegistry.launch_benchmark(context)
+    def test_memory_copy_bw_performance(self):
+        """Test mem-copy-bw benchmark."""
+        benchmark_name = 'mem-copy-bw'
+        (benchmark_class,
+         predefine_params) = BenchmarkRegistry._BenchmarkRegistry__select_benchmark(benchmark_name, Platform.CUDA)
+        assert (benchmark_class)
 
-    # Check basic information.
-    assert (benchmark)
-    assert (benchmark.name == 'mem-copy-bw')
-    assert (benchmark.type == BenchmarkType.MICRO)
+        benchmark = benchmark_class(benchmark_name)
 
-    # Check results and metrics.
-    assert (benchmark.run_count == 1)
-    assert (benchmark.return_code == ReturnCode.SUCCESS)
+        ret = benchmark._preprocess()
+        assert (ret is True)
+        assert (benchmark.return_code == ReturnCode.SUCCESS)
 
-    assert ('raw_output_0' in benchmark.raw_data)
-    assert (len(benchmark.raw_data['raw_output_0']) == 1)
-    assert (isinstance(benchmark.raw_data['raw_output_0'][0], str))
-    for metric in ['H2D_Mem_BW', 'D2H_Mem_BW', 'D2D_Mem_BW']:
-        assert (metric in benchmark.result)
-        assert (len(benchmark.result[metric]) == 1)
-        assert (isinstance(benchmark.result[metric][0], numbers.Number))
+        # Check basic information.
+        assert (benchmark)
+        assert (benchmark.name == 'mem-copy-bw')
+        assert (benchmark.type == BenchmarkType.MICRO)
+
+        # Check command list
+        expected_command = ['bandwidthTest --htod', 'bandwidthTest --dtoh', 'bandwidthTest --dtod']
+        for i in range(len(expected_command)):
+            commnad = benchmark._bin_name + benchmark._commands[i].split(benchmark._bin_name)[1]
+            assert (commnad == expected_command[i])
+
+        # Check results and metrics.
+        raw_output = {}
+        raw_output[0] = """
+    [CUDA Bandwidth Test] - Starting...
+    Running on...
+
+    Device 0: A100-SXM4-40GB
+    Quick Mode
+
+    Host to Device Bandwidth, 1 Device(s)
+    PINNED Memory Transfers
+    Transfer Size (Bytes)	Bandwidth(GB/s)
+    32000000			26.1
+
+    Result = PASS
+    """
+        raw_output[1] = """
+    [CUDA Bandwidth Test] - Starting...
+    Running on...
+
+    Device 0: A100-SXM4-40GB
+    Quick Mode
+
+    Device to Host Bandwidth, 1 Device(s)
+    PINNED Memory Transfers
+    Transfer Size (Bytes)	Bandwidth(GB/s)
+    32000000			23.5
+
+    Result = PASS
+    """
+        raw_output[2] = """
+    NOTE: The CUDA Samples are not meant for performance measurements. Results may vary when GPU Boost is enabled.
+    [CUDA Bandwidth Test] - Starting...
+    Running on...
+
+    Device 0: A100-SXM4-40GB
+    Quick Mode
+
+    Device to Device Bandwidth, 1 Device(s)
+    PINNED Memory Transfers
+    Transfer Size (Bytes)	Bandwidth(GB/s)
+    32000000			1113.3
+
+    Result = PASS
+    """
+        for i, metric in enumerate(['H2D_Mem_BW', 'D2H_Mem_BW', 'D2D_Mem_BW']):
+            assert (benchmark._process_raw_result(i, raw_output[i]))
+            assert (metric in benchmark.result)
+            assert (len(benchmark.result[metric]) == 1)
+            assert (isinstance(benchmark.result[metric][0], numbers.Number))
