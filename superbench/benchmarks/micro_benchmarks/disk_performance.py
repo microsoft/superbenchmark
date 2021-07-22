216813bb28941f2a6db360e0380fbd798443056c
Ziyue Yang <ziyyang@microsoft.com>
2021-07-22 08:41:45 +0000

    address comments

--- superbench/benchmarks/micro_benchmarks/disk_performance.py
+++ superbench/benchmarks/micro_benchmarks/disk_performance.py
@@ -13,7 +13,7 @@ from superbench.benchmarks.micro_benchmarks import MicroBenchmarkWithInvoke
 
 
 class DiskPerformance(MicroBenchmarkWithInvoke):
-    """The KernelLaunch overhead benchmark class."""
+    """The disk performance benchmark class."""
     def __init__(self, name, parameters=''):
         """Constructor.
 
@@ -25,38 +25,38 @@ class DiskPerformance(MicroBenchmarkWithInvoke):
 
         self._bin_name = 'fio'
 
-        self._io_patterns = ['seq', 'rand']
-        self._io_types = ['read', 'write']
-        self._rand_block_size = 4 * 1024    # 4KiB
-        self._seq_block_size = 128 * 1024    # 128KiB
-        self._default_iodepth = 64
-        self._default_ramp_time = 10
-        self._default_runtime = 60
-        self._default_numjobs_for_rand = 4
+        self.__io_patterns = ['seq', 'rand']
+        self.__io_types = ['read', 'write']
+        self.__rand_block_size = 4 * 1024    # 4KiB
+        self.__seq_block_size = 128 * 1024    # 128KiB
+        self.__default_iodepth = 64
+        self.__default_ramp_time = 10
+        self.__default_runtime = 60
+        self.__default_numjobs_for_rand = 4
 
-        self._common_fio_args =\
+        self.__common_fio_args =\
             ' --randrepeat=1 --thread=1 --ioengine=libaio --direct=1'\
             ' --norandommap=1 --lat_percentiles=1 --group_reporting=1'\
             ' --output-format=json'
-        self._fio_args = {}
+        self.__fio_args = {}
 
         # Sequentially write 128KiB to the device twice
-        self._fio_args['seq_precond'] = self._common_fio_args +\
+        self.__fio_args['seq_precond'] = self.__common_fio_args +\
             ' --name=seq_precond --rw=write --bs=%d --iodepth=%d --numjobs=1 --loops=2' %\
-            (self._seq_block_size, self._default_iodepth)
+            (self.__seq_block_size, self.__default_iodepth)
 
         # Randomly write 4KiB to the device
-        self._fio_args['rand_precond'] = self._common_fio_args +\
+        self.__fio_args['rand_precond'] = self.__common_fio_args +\
             ' --name=rand_precond --rw=randwrite --bs=%d --iodepth=%d --numjobs=%d --time_based=1' %\
-            (self._rand_block_size, self._default_iodepth, self._default_numjobs_for_rand)
+            (self.__rand_block_size, self.__default_iodepth, self.__default_numjobs_for_rand)
 
         # Seq/rand read/write tests
-        for io_pattern in self._io_patterns:
-            for io_type in self._io_types:
+        for io_pattern in self.__io_patterns:
+            for io_type in self.__io_types:
                 io_str = '%s_%s' % (io_pattern, io_type)
                 fio_rw = io_type if io_pattern == 'seq' else io_pattern + io_type
-                fio_bs = self._seq_block_size if io_pattern == 'seq' else self._rand_block_size
-                self._fio_args[io_str] = self._common_fio_args +\
+                fio_bs = self.__seq_block_size if io_pattern == 'seq' else self.__rand_block_size
+                self.__fio_args[io_str] = self.__common_fio_args +\
                     ' --name=%s --rw=%s --bs=%d --time_based=1' % (io_str, fio_rw, fio_bs)
 
     def add_parser_arguments(self):
@@ -88,18 +88,18 @@ class DiskPerformance(MicroBenchmarkWithInvoke):
             help='Time in seconds to run rand write precondition. Set to 0 to disable this test.',
         )
 
-        for io_pattern in self._io_patterns:
-            for io_type in self._io_types:
+        for io_pattern in self.__io_patterns:
+            for io_type in self.__io_types:
                 io_str = '%s_%s' % (io_pattern, io_type)
                 self._parser.add_argument(
                     '--%s_ramp_time' % io_str,
                     type=int,
-                    default=self._default_ramp_time,
+                    default=self.__default_ramp_time,
                     required=False,
                     help='Time in seconds to warm up %s test.' % io_str,
                 )
                 # Disable write tests by default
-                default_runtime = 0 if io_type == 'write' else self._default_runtime
+                default_runtime = 0 if io_type == 'write' else self.__default_runtime
                 self._parser.add_argument(
                     '--%s_runtime' % io_str,
                     type=int,
@@ -110,11 +110,11 @@ class DiskPerformance(MicroBenchmarkWithInvoke):
                 self._parser.add_argument(
                     '--%s_iodepth' % io_str,
                     type=int,
-                    default=self._default_iodepth,
+                    default=self.__default_iodepth,
                     required=False,
                     help='Queue depth for each thread in %s test.' % io_str,
                 )
-                default_numjobs = 1 if io_pattern == 'seq' else self._default_numjobs_for_rand
+                default_numjobs = 1 if io_pattern == 'seq' else self.__default_numjobs_for_rand
                 self._parser.add_argument(
                     '--%s_numjobs' % io_str,
                     type=int,
@@ -143,18 +143,18 @@ class DiskPerformance(MicroBenchmarkWithInvoke):
             if self._args.enable_seq_precond:
                 command = fio_path +\
                     ' --filename=%s' % block_device +\
-                    self._fio_args['seq_precond']
+                    self.__fio_args['seq_precond']
                 self._commands.append(command)
 
             if self._args.rand_precond_time > 0:
                 command = fio_path +\
                     ' --filename=%s' % block_device +\
                     ' --runtime=%ds' % self._args.rand_precond_time +\
-                    self._fio_args['rand_precond']
+                    self.__fio_args['rand_precond']
                 self._commands.append(command)
 
-            for io_pattern in self._io_patterns:
-                for io_type in self._io_types:
+            for io_pattern in self.__io_patterns:
+                for io_type in self.__io_types:
                     io_str = '%s_%s' % (io_pattern, io_type)
                     runtime = getattr(self._args, '%s_runtime' % io_str)
                     if runtime > 0:
@@ -164,7 +164,7 @@ class DiskPerformance(MicroBenchmarkWithInvoke):
                             ' --runtime=%ds' % runtime +\
                             ' --iodepth=%d' % getattr(self._args, '%s_iodepth' % io_str) +\
                             ' --numjobs=%d' % getattr(self._args, '%s_numjobs' % io_str) +\
-                            self._fio_args[io_str]
+                            self.__fio_args[io_str]
                         self._commands.append(command)
 
         return True
@@ -185,6 +185,28 @@ class DiskPerformance(MicroBenchmarkWithInvoke):
 
         try:
             fio_output = json.loads(raw_output)
+
+            jobname = fio_output['jobs'][0]['jobname']
+            block_device = fio_output['global options']['filename']
+            jobname_prefix = 'disk_performance:%s:%s' % (block_device, jobname)
+            lat_units = ['lat_ns', 'lat_us', 'lat_ms']
+
+            bs = fio_output['jobs'][0]['job options']['bs']
+            self._result.add_result('%s:bs' % jobname_prefix, float(bs))
+
+            for io_type in ['read', 'write']:
+                io_type_prefix = '%s:%s' % (jobname_prefix, io_type)
+
+                iops = fio_output['jobs'][0][io_type]['iops']
+                self._result.add_result('%s:iops' % io_type_prefix, float(iops))
+
+                for lat_unit in lat_units:
+                    if lat_unit in fio_output['jobs'][0][io_type]:
+                        lat_unit_prefix = '%s:%s' % (io_type_prefix, lat_unit)
+                        for lat_percentile in ['95.000000', '99.000000', '99.900000']:
+                            lat = fio_output['jobs'][0][io_type][lat_unit]['percentile'][lat_percentile]
+                            self._result.add_result('%s:%s' % (lat_unit_prefix, lat_percentile), float(lat))
+                        break
         except BaseException as e:
             self._result.set_return_code(ReturnCode.MICROBENCHMARK_RESULT_PARSING_FAILURE)
             logger.error(
@@ -193,28 +215,6 @@ class DiskPerformance(MicroBenchmarkWithInvoke):
                 )
             )
             return False
-
-        jobname = fio_output['jobs'][0]['jobname']
-        block_device = fio_output['global options']['filename']
-        jobname_prefix = 'disk_performance:%s:%s' % (block_device, jobname)
-        lat_units = ['lat_ns', 'lat_us', 'lat_ms']
-
-        bs = fio_output['jobs'][0]['job options']['bs']
-        self._result.add_result('%s:bs' % jobname_prefix, float(bs))
-
-        for io_type in ['read', 'write']:
-            io_type_prefix = '%s:%s' % (jobname_prefix, io_type)
-
-            iops = fio_output['jobs'][0][io_type]['iops']
-            self._result.add_result('%s:iops' % io_type_prefix, float(iops))
-
-            for lat_unit in lat_units:
-                if lat_unit in fio_output['jobs'][0][io_type]:
-                    lat_unit_prefix = '%s:%s' % (io_type_prefix, lat_unit)
-                    for lat_percentile in ['95.000000', '99.000000', '99.900000']:
-                        lat = fio_output['jobs'][0][io_type][lat_unit]['percentile'][lat_percentile]
-                        self._result.add_result('%s:%s' % (lat_unit_prefix, lat_percentile), float(lat))
-                    break
 
         return True
 
