ac5801ddad4671b5de1dfa218ed7e54f00eb2f0c
yukirora <v-yujiang@microsoft.com>
2021-07-30 09:48:34 +0800

    add rcom rccl support in microbenchmark

--- examples/benchmarks/nccl_bw_performance.py
+++ examples/benchmarks/nccl_bw_performance.py
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT license.
 
-"""Micro benchmark example for NCCL Bus bandwidth performance.
+"""Micro benchmark example for NCCL/RCCL Bus bandwidth performance.
 
 Commands to run:
   python3 examples/benchmarks/nccl_bw_performance.py
@@ -14,7 +14,10 @@ if __name__ == '__main__':
     context = BenchmarkRegistry.create_benchmark_context(
         'nccl-bw', platform=Platform.CUDA, parameters='--operations allreduce'
     )
-
+    # For ROCM environment, please specify the benchmark name and the platform as the following.
+    # context = BenchmarkRegistry.create_benchmark_context(
+    #     'rccl-bw', platform=Platform.ROCM, parameters='--operations allreduce --maxbytes 128M'
+    # )
     benchmark = BenchmarkRegistry.launch_benchmark(context)
     if benchmark:
         logger.info(
