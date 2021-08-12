9c8e88bf9e61e9c99508a5e9fd42b14a5ed0e3ea
Yifan Xiong <yifan.xiong@microsoft.com>
2021-08-12 09:08:33 +0000

    Add vscode and codespaces config for develop
    Add vscode and codespaces config for develop.

---
+++ .devcontainer/post-create-command.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+export PATH=${HOME}/.local/bin:${PATH}
+
+# install python packages
+pip install .[dev,test,torch] --user --no-cache-dir --progress-bar=off --use-feature=in-tree-build
+pre-commit install --install-hooks
+
+# install nodejs packages
+cd website
+npm install --no-progress
+cd -
+
+# try superbench cli
+sb
