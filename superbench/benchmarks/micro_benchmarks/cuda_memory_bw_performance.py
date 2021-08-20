ae86c616530ca78925db8bac8bcb0ed8913ecf84
yukirora <v-yujiang@microsoft.com>
2021-08-20 15:42:32 +0800

    extract base class for mem-bw

--- superbench/benchmarks/micro_benchmarks/cuda_memory_bw_performance.py
+++ superbench/benchmarks/micro_benchmarks/cuda_memory_bw_performance.py
@@ -7,11 +7,11 @@ import os
 import re
 
 from superbench.common.utils import logger
-from superbench.benchmarks import BenchmarkRegistry, Platform, ReturnCode
-from superbench.benchmarks.micro_benchmarks import MicroBenchmarkWithInvoke
+from superbench.benchmarks import BenchmarkRegistry, Platform
+from superbench.benchmarks.micro_benchmarks import MemBwBenchmark
 
 
-class CudaMemBwBenchmark(MicroBenchmarkWithInvoke):
+class CudaMemBwBenchmark(MemBwBenchmark):
     """The Cuda memory performance benchmark class."""
     def __init__(self, name, parameters=''):
         """Constructor.
@@ -23,30 +23,15 @@ class CudaMemBwBenchmark(MicroBenchmarkWithInvoke):
         super().__init__(name, parameters)
 
         self._bin_name = 'bandwidthTest'
-        self.__mem_types = ['htod', 'dtoh', 'dtod']
-        self.__memory = ['pageable', 'pinned']
 
     def add_parser_arguments(self):
         """Add the specified arguments."""
         super().add_parser_arguments()
         self._parser.add_argument(
-            '--mem_type',
-            type=str,
-            nargs='+',
-            default=self.__mem_types,
-            help='Memory types to benchmark. E.g. {}.'.format(' '.join(self.__mem_types)),
-        )
-        self._parser.add_argument(
             '--shmoo_mode',
             action='store_true',
             default=False,
             help='Enable shmoo mode for bandwidthtest.',
-        )
-        self._parser.add_argument(
-            '--memory',
-            type=str,
-            default=None,
-            help='Memory argument for bandwidthtest. E.g. {}.'.format(' '.join(self.__memory)),
         )
 
     def _preprocess(self):
@@ -58,38 +43,16 @@ class CudaMemBwBenchmark(MicroBenchmarkWithInvoke):
         if not super()._preprocess():
             return False
 
-        # Format the arguments
-        if not isinstance(self._args.mem_type, list):
-            self._args.mem_type = [self._args.mem_type]
-        self._args.mem_type = [p.lower() for p in self._args.mem_type]
-
         # Check the arguments and generate the commands
         for mem_type in self._args.mem_type:
-            if mem_type not in self.__mem_types:
-                self._result.set_return_code(ReturnCode.INVALID_ARGUMENT)
-                logger.error(
-                    'Unsupported mem_type of bandwidth test - benchmark: {}, mem_type: {}, expected: {}.'.format(
-                        self._name, mem_type, ' '.join(self.__mem_types)
-                    )
-                )
-                return False
-            else:
-                command = os.path.join(self._args.bin_dir, self._bin_name)
-                command += ' --' + mem_type
-                if self._args.shmoo_mode:
-                    command += ' mode=shmoo'
-                if self._args.memory:
-                    if self._args.memory in self.__memory:
-                        command += ' memory=' + self._args.memory
-                    else:
-                        self._result.set_return_code(ReturnCode.INVALID_ARGUMENT)
-                        logger.error(
-                            'Unsupported memory argument of bandwidth test - benchmark: {}, memory: {}, expected: {}.'.
-                            format(self._name, self._args.memory, ' '.join(self.__memory))
-                        )
-                        return False
-                command += ' --csv'
-                self._commands.append(command)
+            command = os.path.join(self._args.bin_dir, self._bin_name)
+            command += ' --' + mem_type
+            if self._args.shmoo_mode:
+                command += ' mode=shmoo'
+            if self._args.memory == 'pinned':
+                command += ' memory=pinned'
+            command += ' --csv'
+            self._commands.append(command)
 
         return True
 
@@ -108,23 +71,16 @@ class CudaMemBwBenchmark(MicroBenchmarkWithInvoke):
         self._result.add_raw_data('raw_output_' + self._args.mem_type[cmd_idx], raw_output)
 
         mem_bw = -1
-        metric = ''
         valid = True
         content = raw_output.splitlines()
         try:
-            for index, line in enumerate(content):
-                if 'H2D' in line:
-                    metric = 'H2D_Mem_BW'
-                elif 'D2H' in line:
-                    metric = 'D2H_Mem_BW'
-                elif 'D2D' in line:
-                    metric = 'D2D_Mem_BW'
-                else:
-                    continue
-                line = line.split(',')[1]
-                value = re.search(r'(\d+.\d+)', line)
-                if value:
-                    mem_bw = max(mem_bw, float(value.group(0)))
+            metric = self._metrics[self._mem_types.index(self._args.mem_type[cmd_idx])]
+            for line in content:
+                if 'D2H' in line or 'H2D' in line:
+                    line = line.split(',')[1]
+                    value = re.search(r'(\d+.\d+)', line)
+                    if value:
+                        mem_bw = max(mem_bw, float(value.group(0)))
 
         except BaseException:
             valid = False
---
+++ superbench/benchmarks/micro_benchmarks/memory_bw_performance_base.py
@@ -0,0 +1,74 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+"""Module of the memory performance benchmarks base class."""
+
+from superbench.common.utils import logger
+from superbench.benchmarks import ReturnCode
+from superbench.benchmarks.micro_benchmarks import MicroBenchmarkWithInvoke
+
+
+class MemBwBenchmark(MicroBenchmarkWithInvoke):
+    """The Cuda memory performance benchmark class."""
+    def __init__(self, name, parameters=''):
+        """Constructor.
+
+        Args:
+            name (str): benchmark name.
+            parameters (str): benchmark parameters.
+        """
+        super().__init__(name, parameters)
+
+        self._mem_types = ['htod', 'dtoh']
+        self._metrics = ['H2D_Mem_BW', 'D2H_Mem_BW']
+        self._memory = ['pinned', 'unpinned']
+
+    def add_parser_arguments(self):
+        """Add the specified arguments."""
+        super().add_parser_arguments()
+        self._parser.add_argument(
+            '--mem_type',
+            type=str,
+            nargs='+',
+            default=self._mem_types,
+            help='Memory types to benchmark. E.g. {}.'.format(' '.join(self._mem_types)),
+        )
+        self._parser.add_argument(
+            '--memory',
+            type=str,
+            default='pinned',
+            help='Memory argument for bandwidthtest. E.g. {}.'.format(' '.join(self._memory)),
+        )
+
+    def _preprocess(self):
+        """Preprocess/preparation operations before the benchmarking.
+
+        Return:
+            True if _preprocess() succeed.
+        """
+        if not super()._preprocess():
+            return False
+
+        # Format the arguments
+        self._args.mem_type = [p.lower() for p in self._args.mem_type]
+
+        # Check the arguments and generate the commands
+        if self._args.memory not in self._memory:
+            self._result.set_return_code(ReturnCode.INVALID_ARGUMENT)
+            logger.error(
+                'Unsupported mem_type of bandwidth test - benchmark: {}, memory: {}, expected: {}.'.format(
+                    self._name, self._args.memory, ' '.join(self._memory)
+                )
+            )
+            return False
+        for mem_type in self._args.mem_type:
+            if mem_type not in self._mem_types:
+                self._result.set_return_code(ReturnCode.INVALID_ARGUMENT)
+                logger.error(
+                    'Unsupported mem_type of bandwidth test - benchmark: {}, mem_type: {}, expected: {}.'.format(
+                        self._name, mem_type, ' '.join(self._mem_types)
+                    )
+                )
+                return False
+
+        return True
