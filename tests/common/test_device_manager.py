ab87906b4e990877984567444d57915e87c5eb88
Guoshuai Zhao <guzhao@microsoft.com>
2021-11-12 19:54:24 +0900

    fix lazy import

--- tests/common/test_device_manager.py
+++ tests/common/test_device_manager.py
@@ -7,23 +7,23 @@ import numbers
 from unittest import mock
 
 from tests.helper import decorator
+import superbench.common.utils.device_manager as dm
 
 
 @decorator.cuda_test
 @mock.patch('superbench.common.utils.process.run_command')
 def test_nvidia_helper_utils(mock_run_command):
     """Test util functions of nvidia_helper."""
-    from superbench.common.utils.device_manager import device_manager
-    assert (isinstance(device_manager.get_device_count(), numbers.Number))
-    assert (isinstance(device_manager.get_device_compute_capability(), numbers.Number))
-    assert (isinstance(device_manager.get_device_utilization(0), numbers.Number))
-    assert (isinstance(device_manager.get_device_temperature(0), numbers.Number))
-    assert (isinstance(device_manager.get_device_power_limit(0), numbers.Number))
+    assert (isinstance(dm.device_manager.get_device_count(), numbers.Number))
+    assert (isinstance(dm.device_manager.get_device_compute_capability(), numbers.Number))
+    assert (isinstance(dm.device_manager.get_device_utilization(0), numbers.Number))
+    assert (isinstance(dm.device_manager.get_device_temperature(0), numbers.Number))
+    assert (isinstance(dm.device_manager.get_device_power_limit(0), numbers.Number))
 
-    used_mem, total_mem = device_manager.get_device_memory(0)
+    used_mem, total_mem = dm.device_manager.get_device_memory(0)
     assert (isinstance(used_mem, numbers.Number) and isinstance(total_mem, numbers.Number))
 
-    corrected_ecc, uncorrected_ecc = device_manager.get_device_ecc_error(0)
+    corrected_ecc, uncorrected_ecc = dm.device_manager.get_device_ecc_error(0)
     assert (isinstance(corrected_ecc, numbers.Number) and isinstance(uncorrected_ecc, numbers.Number))
     mock_run_command.return_value.returncode = 0
     mock_run_command.return_value.stdout = """
@@ -41,7 +41,7 @@ def test_nvidia_helper_utils(mock_run_command):
         Temperature
             GPU Current Temp                  : 36 C
     """
-    gpu_remapped_info = device_manager.get_device_row_remapped_info(0)
+    gpu_remapped_info = dm.device_manager.get_device_row_remapped_info(0)
     expected = {
         'gpu_remap_correctable_error': 0,
         'gpu_remap_uncorrectable_error': 0,
