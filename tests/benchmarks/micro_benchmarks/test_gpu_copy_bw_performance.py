b6781968f2325de097d5e392f3efa3dee3318edc
Ziyue Yang <ziyyang@microsoft.com>
2021-12-13 15:02:39 +0800

    Benchmarks: Fix Comment - Correct benchmark name in test_gpu_copy_bw_performance.py #263
    **Description**
    Benchmarks: Fix Comment - Correct benchmark name in test_gpu_copy_bw_performance.py.

--- tests/benchmarks/micro_benchmarks/test_gpu_copy_bw_performance.py
+++ tests/benchmarks/micro_benchmarks/test_gpu_copy_bw_performance.py
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-"""Tests for disk-performance benchmark."""
+"""Tests for gpu-copy-bw benchmark."""
 
 from pathlib import Path
 import numbers
