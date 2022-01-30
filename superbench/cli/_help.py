28195be6db5e9e4188ed3c0e4fe69c3c14a0cea0
Yuting Jiang <v-yutjiang@microsoft.com>
2022-01-30 13:38:00 +0800

    Bug - Fix typo in document (#297)
    Fix typo in document.

--- superbench/cli/_help.py
+++ superbench/cli/_help.py
@@ -114,14 +114,14 @@ helps['result diagnosis'] = """
             --data-file outputs/results-summary.jsonl
             --rule-file rule.yaml
             --baseline-file baseline.json
-            --output-file-foramt excel
+            --output-file-format excel
         - name: run data diagnosis and output the results in jsonl format
           text: >
             {cli_name} result diagnosis
             --data-file outputs/results-summary.jsonl
             --rule-file rule.yaml
             --baseline-file baseline.json
-            --output-file-foramt json
+            --output-file-format json
 """.format(cli_name=CLI_NAME)
 
 
