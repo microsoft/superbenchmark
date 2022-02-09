020339a8a52c40bed1087cf67f69553aec3b6252
454314380 <454314380@qq.com>
2022-02-09 17:02:49 +0800

    use api to detect version

--- superbench/benchmarks/model_benchmarks/pytorch_base.py
+++ superbench/benchmarks/model_benchmarks/pytorch_base.py
@@ -5,7 +5,6 @@
 
 import os
 from datetime import timedelta
-import re
 
 import torch
 import transformers
@@ -68,13 +67,14 @@ class PytorchBase(ModelBenchmark):
                         ' distributed implementation: {}.'.format(self._name, self._args.distributed_impl)
                     )
                     return False
-
-                if self.version_larger(str(torch.__version__), '1.9.0'):
+                try:
+                    from torch.distributed import elastic    # noqa F401
+                    # >= 1.9.0a0
                     port = int(os.environ['MASTER_PORT']) + 1
                     addr = os.environ['MASTER_ADDR']
                     rank = int(os.environ['RANK'])
                     world_size = int(os.environ['WORLD_SIZE'])
-                    logger.debug('ip:{},port:{},rank:{},world:{}'.format(addr, str(port), str(rank), str(world_size)))
+                    logger.debug('ip:{},port:{},rank:{},world:{}'.format(addr, port, rank, world_size))
                     store = PrefixStore(self._name, TCPStore(addr, port, world_size, rank == 0, timedelta(seconds=300)))
                     torch.distributed.init_process_group(
                         backend=self._args.distributed_backend.value,
@@ -83,7 +83,8 @@ class PytorchBase(ModelBenchmark):
                         world_size=world_size,
                         store=store
                     )
-                else:
+                except ImportError:
+                    # < 1.9.0a0
                     torch.distributed.init_process_group(backend=self._args.distributed_backend.value)
                 self._world_size = int(os.environ['WORLD_SIZE'])
                 self._local_rank = int(os.environ['LOCAL_RANK'])
@@ -259,20 +260,3 @@ class PytorchBase(ModelBenchmark):
             The count of trainable parameters.
         """
         return sum(p.numel() for p in self._model.parameters() if p.requires_grad)
-
-    def version_larger(self, x, y):
-        """Detect if pytorch version x >= y.
-
-        Args:
-            x (str): pytorch version
-            y (str): pytorch version
-
-        Returns:
-            bool: True if x >= y, otherwise False
-        """
-        x = [int(i) for i in re.match(r'\d+.\d+.\d+', x).group().split('.')]
-        y = [int(i) for i in re.match(r'\d+.\d+.\d+', y).group().split('.')]
-        if x[0] > y[0] or (x[0] == y[0] and x[1] > y[1]) \
-                or (x[0] == y[0] and x[1] == y[1] and x[2] >= y[2]):
-            return True
-        return False
