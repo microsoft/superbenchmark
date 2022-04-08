56c9a711a822be3c1303a93c1f4c9e1bab42568e
Yuting Jiang <454314380@qq.com>
2022-04-09 04:44:25 +0800

    Docs - Add usage for result summary  (#337)
    **Description**
    Add usage for result summary.

--- website/sidebars.js
+++ website/sidebars.js
@@ -32,6 +32,7 @@ module.exports = {
         },
         'user-tutorial/system-config',
         'user-tutorial/data-diagnosis',
+        'user-tutorial/result-summary',
         'user-tutorial/monitor',
         'user-tutorial/container-images',
       ],
