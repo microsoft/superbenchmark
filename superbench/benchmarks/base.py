e75c105924108bdc8f191ed7387de091f258e058
Yifan Xiong <yifan.xiong@microsoft.com>
2022-08-25 19:37:47 +0800

    Enhance parameter parsing to allow spaces in value (#397)
    Enhance parameter parsing to support string like `"--arg1 value --arg2 'a long string with spaces'"`.

--- superbench/benchmarks/base.py
+++ superbench/benchmarks/base.py
@@ -3,6 +3,7 @@
 
 """Module of the base class."""
 
+import shlex
 import signal
 import traceback
 import argparse
@@ -39,7 +40,7 @@ class Benchmark(ABC):
             parameters (str): benchmark parameters.
         """
         self._name = name
-        self._argv = list(filter(None, parameters.split(' '))) if parameters is not None else list()
+        self._argv = list(filter(None, shlex.split(parameters))) if parameters is not None else list()
         self._benchmark_type = None
         self._parser = argparse.ArgumentParser(
             add_help=False,
