44980077c4bfcf11b1655606387edbaa326107d2
Yang Wang <yangwang1@microsoft.com>
2022-09-02 14:00:38 +0800

    fix

--- superbench/common/utils/topo_aware.py
+++ superbench/common/utils/topo_aware.py
@@ -44,7 +44,8 @@ def gen_ibstat_file(host_list, ibstat_file):
     """
     try:
         pssh_cmd = "pssh -i -t 5 -H '{}' ".format(' '.join(host_list))
-        cmd = "'cat /sys/class/infiniband/*/sys_image_guid | tr -d :' | sed 's/^.*[[[:upper:]]\{7\}]/VM_hostname/g' | cut -d ' ' -f 1,2"
+        cmd = "'cat /sys/class/infiniband/*/sys_image_guid" \
+            r"| tr -d :' | sed 's/^.*[[[:upper:]]\{7\}]/VM_hostname/g' | cut -d ' ' -f 1,2"
         output = os.popen(pssh_cmd + cmd).read()
         # Generate ibstat file
         ibstate_file_path = Path(ibstat_file)
