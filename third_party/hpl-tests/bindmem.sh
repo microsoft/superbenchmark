9faf5092aab37ff825dba2aa97d2a62b4e4df23d
Ubuntu <azureuser@sbtestvm.jzlku1oskncengjiado35wf1hd.ax.internal.cloudapp.net>
2023-02-24 16:10:53 +0000

    adding copyright message

--- third_party/hpl-tests/bindmem.sh
+++ third_party/hpl-tests/bindmem.sh
@@ -1,3 +1,5 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
 #!/bin/bash
 nodes=$(numactl --show | awk -F: '/^cpubind/ {print $2;}' | sed -e 's/^ //g' -e 's/ $//g' | tr ' ' ',')
 exec numactl --interleave=${nodes} $@
--- third_party/hpl-tests/hpl_run.sh
+++ third_party/hpl-tests/hpl_run.sh
@@ -1,3 +1,5 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
 #!/bin/bash
 
 source /opt/hpcx/hpcx-init.sh
