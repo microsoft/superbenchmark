4cb431cab4dfc43f61e4adf7712fb3e9ebe48e25
Ziyue Yang <ziyyang@microsoft.com>
2023-04-24 10:17:49 +0800

    Benchmarks - Revise step time collection in distributed inference benchmark (#524)
    **Description**
    This commit revises distributed inference benchmark to give a unified
    step time result by taking maximum step times of different GPUs.

--- superbench/benchmarks/micro_benchmarks/dist_inference.py
+++ superbench/benchmarks/micro_benchmarks/dist_inference.py
@@ -14,6 +14,7 @@ from superbench.common.utils import logger
 from superbench.benchmarks import DistributedImpl, DistributedBackend, BenchmarkRegistry, ReturnCode, Precision
 from superbench.benchmarks.micro_benchmarks import MicroBenchmark
 from superbench.benchmarks.context import Enum
+from superbench.benchmarks.reducer import ReduceType
 
 
 class ComputationKernelType(Enum):
@@ -390,7 +391,7 @@ class DistInference(MicroBenchmark):
         Return:
             True if _process_data succeeds.
         """
-        if not self._process_numeric_result('step_times', step_times, cal_percentile=True):
+        if not self._process_numeric_result('step_times', step_times, reduce_type=ReduceType.MAX, cal_percentile=True):
             return False
         return True
 
