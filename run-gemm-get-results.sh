9d5e1a6a846e252af2b09807a74ec208ca974ca7
Ubuntu <ficoguti@a100-fg.ldhnrgqrdb2unibdeiogli1xfh.bx.internal.cloudapp.net>
2023-09-20 20:46:03 +0000

    cleanup

--- run-gemm-get-results.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source ~/workspace/sb/bin/activate
-
-sb run -f local.ini -c gemm.yaml --output-dir outputs/gemm
-
-sb result summary --data-file outputs/gemm/results-summary.jsonl --rule-file gemm-rule.yaml --output-dir outputs/gemm --output-file-format md
--- run-gemms.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ~/workspace/sb/bin/activate
-
-sb run -f local.ini -c simple-gemm.yaml
