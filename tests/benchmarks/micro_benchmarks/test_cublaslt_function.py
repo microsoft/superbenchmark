a69c3254e15dcbd9a2712089edab1dd089483fe8
yukirora <yuting.jiang@microsoft.com>
2023-11-17 06:40:04 +0000

    add test

--- tests/benchmarks/micro_benchmarks/test_cublaslt_function.py
+++ tests/benchmarks/micro_benchmarks/test_cublaslt_function.py
@@ -63,7 +63,7 @@ class CublasLtBenchmarkTestCase(BenchmarkTestCase, unittest.TestCase):
         (benchmark_cls, _) = BenchmarkRegistry._BenchmarkRegistry__select_benchmark(self.benchmark_name, Platform.CUDA)
         benchmark = benchmark_cls(
             self.benchmark_name,
-            parameters='--batch 2:16:2 --shapes 2:4,4:8,8:32 32:128:4,128,128 --in_types fp16 fp32 fp64',
+            parameters='--batch 2:16:2 --shapes 2:4,4:8,8:32 32:128:4,128,128 --in_types fp16 fp32 fp64 int8',
         )
         self.assertTrue(benchmark._preprocess())
         self.assertEqual(4 * (2 * 2 * 3 + 2) * 3, len(benchmark._commands))
@@ -71,7 +71,7 @@ class CublasLtBenchmarkTestCase(BenchmarkTestCase, unittest.TestCase):
         def cmd(t, b, m, n, k):
             return f'{benchmark._CublasLtBenchmark__bin_path} -m {m} -n {n} -k {k} -b {b} -w 20 -i 50 -t {t}'
 
-        for _t in ['fp16', 'fp32', 'fp64']:
+        for _t in ['fp16', 'fp32', 'fp64', 'int8']:
             for _b in [2, 4, 8, 16]:
                 for _m in [2, 4]:
                     for _n in [4, 8]:
