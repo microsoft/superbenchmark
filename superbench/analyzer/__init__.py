9f4880cb8ef56b0ef900ad11d28b51b27994a526
guoshzhao <guzhao@microsoft.com>
2023-11-22 14:42:32 +0800

    Analyzer - Generate baseline given results from multiple nodes.  (#575)
    **Description**
    Generate baseline given results from multiple nodes. 
    
    **Major Revision**
    - Add sub command `sb result generate-baseline`
    - Add UT and docs
    
    ---------
    
    Co-authored-by: 454314380 <454314380@qq.com>
    Co-authored-by: Yuting Jiang <yutingjiang@microsoft.com>

--- superbench/analyzer/__init__.py
+++ superbench/analyzer/__init__.py
@@ -8,5 +8,9 @@ from superbench.analyzer.data_diagnosis import DataDiagnosis
 from superbench.analyzer.diagnosis_rule_op import RuleOp, DiagnosisRuleType
 from superbench.analyzer.summary_op import SummaryOp, SummaryType
 from superbench.analyzer.result_summary import ResultSummary
+from superbench.analyzer.baseline_generation import BaselineGeneration
 
-__all__ = ['DataDiagnosis', 'DiagnosisRuleType', 'RuleOp', 'RuleBase', 'SummaryOp', 'SummaryType', 'ResultSummary']
+__all__ = [
+    'DataDiagnosis', 'DiagnosisRuleType', 'RuleOp', 'RuleBase', 'SummaryOp', 'SummaryType', 'ResultSummary',
+    'BaselineGeneration'
+]
