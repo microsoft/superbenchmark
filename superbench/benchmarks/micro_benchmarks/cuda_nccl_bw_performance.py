e94e4a11fb9cd776d75804edd78d885ec6a9a6d8
Ziyue Yang <ziyyang@microsoft.com>
2023-12-04 14:34:42 +0000

    fix lint

--- superbench/benchmarks/micro_benchmarks/cuda_nccl_bw_performance.py
+++ superbench/benchmarks/micro_benchmarks/cuda_nccl_bw_performance.py
@@ -125,8 +125,7 @@ class CudaNcclBwBenchmark(MicroBenchmarkWithInvoke):
             command = os.path.join(self._args.bin_dir, self._bin_name)
             command += ' -b {} -e {} -f {} -g {} -c {} -n {} -w {} -G {}'.format(
                 self._args.minbytes, self._args.maxbytes, str(self._args.stepfactor), str(self._args.ngpus),
-                str(self._args.check), str(self._args.iters), str(self._args.warmup_iters),
-                str(self._args.graph_iters)
+                str(self._args.check), str(self._args.iters), str(self._args.warmup_iters), str(self._args.graph_iters)
             )
             self._commands.append(command)
 
