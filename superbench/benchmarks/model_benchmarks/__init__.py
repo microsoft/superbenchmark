dd5a6329ed6e812de1035933af47b72c4f6155e2
Yuting Jiang <yutingjiang@microsoft.com>
2023-12-07 09:37:09 +0800

    Benchmarks: Add benchmark: Megatron-LM/Megatron-Deepspeed GPT pretrain benchmark (#582)
    **Description**
    Megatron-LM/Megatron-Deepspeed GPT pretrain benchmark

--- superbench/benchmarks/model_benchmarks/__init__.py
+++ superbench/benchmarks/model_benchmarks/__init__.py
@@ -8,5 +8,6 @@ from superbench.benchmarks.model_benchmarks.pytorch_bert import PytorchBERT
 from superbench.benchmarks.model_benchmarks.pytorch_gpt2 import PytorchGPT2
 from superbench.benchmarks.model_benchmarks.pytorch_cnn import PytorchCNN
 from superbench.benchmarks.model_benchmarks.pytorch_lstm import PytorchLSTM
+from superbench.benchmarks.model_benchmarks.megatron_gpt3 import MegatronGPT
 
-__all__ = ['ModelBenchmark', 'PytorchBERT', 'PytorchGPT2', 'PytorchCNN', 'PytorchLSTM']
+__all__ = ['ModelBenchmark', 'PytorchBERT', 'PytorchGPT2', 'PytorchCNN', 'PytorchLSTM', 'MegatronGPT']
