ea62c54f95b6475a151e5f76bdf89b1f0c283119
Ziyue Yang <ziyyang@microsoft.com>
2023-12-07 12:51:48 +0000

    fix lint

--- examples/benchmarks/gpu_copy_bw_performance.py
+++ examples/benchmarks/gpu_copy_bw_performance.py
@@ -12,7 +12,9 @@ from superbench.common.utils import logger
 
 if __name__ == '__main__':
     context = BenchmarkRegistry.create_benchmark_context(
-        'gpu-copy-bw', platform=Platform.CUDA, parameters='--mem_type htod dtoh dtod one_to_all all_to_one all_to_all --copy_type sm dma'
+        'gpu-copy-bw',
+        platform=Platform.CUDA,
+        parameters='--mem_type htod dtoh dtod one_to_all all_to_one all_to_all --copy_type sm dma'
     )
     # For ROCm environment, please specify the benchmark name and the platform as the following.
     # context = BenchmarkRegistry.create_benchmark_context(
