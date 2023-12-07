06a1cfc787a761fd37109089dae38bafd31c7618
Ziyue Yang <ziyyang@microsoft.com>
2023-12-07 07:56:45 +0000

    fix lint

--- examples/benchmarks/dist_inference_cpp.py
+++ examples/benchmarks/dist_inference_cpp.py
@@ -11,9 +11,7 @@ from superbench.benchmarks import Framework, BenchmarkRegistry
 from superbench.common.utils import logger
 
 if __name__ == '__main__':
-    context = BenchmarkRegistry.create_benchmark_context(
-        'dist-inference-cpp', platform=Platform.CUDA, parameters=''
-    )
+    context = BenchmarkRegistry.create_benchmark_context('dist-inference-cpp', platform=Platform.CUDA, parameters='')
 
     benchmark = BenchmarkRegistry.launch_benchmark(context)
     if benchmark:
