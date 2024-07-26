e304cf15728480ce9986e50fe30a7ca25ee40a3d
Yuting Jiang <yutingjiang@microsoft.com>
2024-07-26 10:42:17 +0800

    Benchmarks: Micro benchmarks - add support for NVIDIA L4/L40/L40s GPUs in gemm-flops (#634)
    **Description**
    Add support GPU ARCH 8.9 for NVIDIA L4/L40/L40s GPUs in gemm-flops.

--- superbench/benchmarks/micro_benchmarks/cuda_gemm_flops_performance.py
+++ superbench/benchmarks/micro_benchmarks/cuda_gemm_flops_performance.py
@@ -47,6 +47,8 @@ class CudaGemmFlopsBenchmark(GemmFlopsBenchmark):
         # Skip FP64 for RTX Turing/Ampere and Tesla T4/GA10x due to very limited FP64 TFLOP rate
         self.__kernel_map[7.5] = {k: self.__kernel_map[7.0][k] for k in self.__kernel_map[7.0] if 'fp64' not in k}
         self.__kernel_map[8.6] = {k: self.__kernel_map[8.0][k] for k in self.__kernel_map[8.0] if 'fp64' not in k}
+        # Skip FP64 for Ada Lovelace L4/L40 due to no native CUDA/Tensor Cores
+        self.__kernel_map[8.9] = {k: self.__kernel_map[8.0][k] for k in self.__kernel_map[8.0] if 'fp64' not in k}
         # Skip INT4 for Hopper due to no native CUDA/Tensor Cores
         self.__kernel_map[9.0] = {k: self.__kernel_map[8.0][k] for k in self.__kernel_map[8.0] if 'int4_tc' not in k}
         self.__parse_logline = [
