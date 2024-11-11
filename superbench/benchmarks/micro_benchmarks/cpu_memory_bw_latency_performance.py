2a06c3d0f384df2043a8f25ee83837953d7392f7
hongtaozhang <hongtaozhang@microsoft.com>
2024-11-10 23:00:41 -0800

    Combine two cpu benchmark classes.

--- superbench/benchmarks/micro_benchmarks/cpu_memory_bw_latency_performance.py
+++ superbench/benchmarks/micro_benchmarks/cpu_memory_bw_latency_performance.py
@@ -11,7 +11,7 @@ from superbench.benchmarks import BenchmarkRegistry, ReturnCode
 from superbench.benchmarks.micro_benchmarks import MicroBenchmarkWithInvoke
 
 
-class CpuMemBwLatencyBenchmarkMlc(MicroBenchmarkWithInvoke):
+class CpuMemBwLatencyBenchmark(MicroBenchmarkWithInvoke):
     """The Memory bandwidth and latency benchmark class."""
     def __init__(self, name, parameters=''):
         """Constructor.
@@ -22,7 +22,7 @@ class CpuMemBwLatencyBenchmarkMlc(MicroBenchmarkWithInvoke):
         """
         super().__init__(name, parameters)
 
-        self._bin_name = 'mlc'
+        self._bin_name = 'mlc' if "x86_64" in platform.machine() else 'cpu_copy'
         self.__support_mlc_commands = ['bandwidth_matrix', 'latency_matrix', 'max_bandwidth']
 
     def add_parser_arguments(self):
@@ -38,15 +38,8 @@ class CpuMemBwLatencyBenchmarkMlc(MicroBenchmarkWithInvoke):
             help='The modes to run mlc with. Possible values are {}.'.format(' '.join(self.__support_mlc_commands))
         )
 
-    def _preprocess(self):
-        """Preprocess/preparation operations before the benchmarking.
-
-        Return:
-            True if _preprocess() succeed.
-        """
-        if not super()._preprocess():
-            return False
-
+    def _preprocess_mlc(self):
+        """ Preprocess/preparation operations for the Intel MLC tool."""
         mlc_path = os.path.join(self._args.bin_dir, self._bin_name)
         ret_val = os.access(mlc_path, os.X_OK | os.F_OK)
         if not ret_val:
@@ -67,18 +60,36 @@ class CpuMemBwLatencyBenchmarkMlc(MicroBenchmarkWithInvoke):
             self._commands.append(mlc_wrapper % command)
         return True
 
-    def _process_raw_result(self, cmd_idx, raw_output):
-        """Function to parse raw results and save the summarized results.
+    def _preprocess_general(self):
+        """Preprocess/preparation operations for the general CPU copy benchmark."""
+        # TODO: enable hugepages?
 
-          self._result.add_raw_data() and self._result.add_result() need to be called to save the results.
+        self.__bin_path = os.path.join(self._args.bin_dir, self._bin_name)
 
-        Args:
-            cmd_idx (int): the index of command corresponding with the raw_output.
-            raw_output (str): raw output string of the micro-benchmark.
+        args = '--size %d --num_warm_up %d --num_loops %d' % (
+            self._args.size, self._args.num_warm_up, self._args.num_loops
+        )
+
+        if self._args.check_data:
+            args += ' --check_data'
+
+        self._commands = ['%s %s' % (self.__bin_path, args)]
+
+        return True
+
+    def _preprocess(self):
+        """Preprocess/preparation operations before the benchmarking.
 
         Return:
-            True if the raw output string is valid and result can be extracted.
+            True if _preprocess() succeed.
         """
+        if not super()._preprocess():
+            return False
+
+        return self._preprocess_mlc() if "x86_64" in platform.machine() else self._preprocess_general()
+
+    def _process_raw_result_mlc(self, cmd_idx, raw_output):
+        """Function to parse raw results for the Intel MLC tool and save the summarized results."""
         self._result.add_raw_data('raw_output_' + str(cmd_idx), raw_output, self._args.log_raw_data)
 
         # parse the command to see which command this output belongs to
@@ -117,7 +128,42 @@ class CpuMemBwLatencyBenchmarkMlc(MicroBenchmarkWithInvoke):
                 else:
                     metric = 'mem_{}_{}_{}_{}'.format(mlc_test, key, str(index), measure).lower()
                 self._result.add_result(metric, float(out_table[key][index]))
+
         return True
+
+    def _process_raw_result_genneral(self, cmd_idx, raw_output):
+        """Function to parse raw results for the general CPU copy benchmark and save the summarized results."""
+        self._result.add_raw_data('raw_output_' + str(cmd_idx), raw_output, self._args.log_raw_data)
+
+        try:
+            for output_line in raw_output.strip().splitlines():
+                self._result.add_result(output_line.strip())
+        except BaseException as e:
+            self._result.set_return_code(ReturnCode.MICROBENCHMARK_RESULT_PARSING_FAILURE)
+            logger.error(
+                'The result format is invalid - round: {}, benchmark: {}, raw output: {}, message: {}.'.format(
+                    self._curr_run_index, self._name, raw_output, str(e)
+                )
+            )
+
+            return False
+
+        return True
+
+    def _process_raw_result(self, cmd_idx, raw_output):
+        """Function to parse raw results and save the summarized results.
+
+          self._result.add_raw_data() and self._result.add_result() need to be called to save the results.
+
+        Args:
+            cmd_idx (int): the index of command corresponding with the raw_output.
+            raw_output (str): raw output string of the micro-benchmark.
+
+        Return:
+            True if the raw output string is valid and result can be extracted.
+        """
+        return self._process_raw_result_mlc(cmd_idx, raw_output) if "x86_64" in platform.machine(
+        ) else self._process_raw_result_genneral(cmd_idx, raw_output)
 
     def _parse_bw_latency(self, raw_output):
         out_table = dict()
@@ -147,107 +193,4 @@ class CpuMemBwLatencyBenchmarkMlc(MicroBenchmarkWithInvoke):
             out_table[key] = [vals[-1]]
         return out_table
 
-
-class CpuCopyBwBenchmarkGeneral(MicroBenchmarkWithInvoke):
-    """The CPU copy bandwidth performance benchmark class."""
-    def __init__(self, name, parameters=''):
-        """Constructor.
-
-        Args:
-            name (str): benchmark name.
-            parameters (str): benchmark parameters.
-        """
-        super().__init__(name, parameters)
-
-        self._bin_name = 'cpu_copy'
-
-    def add_parser_arguments(self):
-        """Add the specified arguments."""
-        super().add_parser_arguments()
-
-        self._parser.add_argument(
-            '--size',
-            type=int,
-            default=256 * 1024**2,
-            required=False,
-            help='Size of data buffer in bytes.',
-        )
-
-        self._parser.add_argument(
-            '--num_warm_up',
-            type=int,
-            default=20,
-            required=False,
-            help='Number of warm up rounds',
-        )
-
-        self._parser.add_argument(
-            '--num_loops',
-            type=int,
-            default=100,
-            required=False,
-            help='Number of data buffer copies performed.',
-        )
-
-        self._parser.add_argument(
-            '--check_data',
-            action='store_true',
-            help='Enable data checking',
-        )
-
-    def _preprocess(self):
-        """Preprocess/preparation operations before the benchmarking.
-
-        Return:
-            True if _preprocess() succeed.
-        """
-        if not super()._preprocess():
-            return False
-
-        # TODO: enable hugepages?
-
-        self.__bin_path = os.path.join(self._args.bin_dir, self._bin_name)
-
-        args = '--size %d --num_warm_up %d --num_loops %d' % (
-            self._args.size, self._args.num_warm_up, self._args.num_loops
-        )
-
-        if self._args.check_data:
-            args += ' --check_data'
-
-        self._commands = ['%s %s' % (self.__bin_path, args)]
-
-        return True
-
-    def _process_raw_result(self, cmd_idx, raw_output):
-        """Function to parse raw results and save the summarized results.
-
-          self._result.add_raw_data() and self._result.add_result() need to be called to save the results.
-
-        Args:
-            cmd_idx (int): the index of command corresponding with the raw_output.
-            raw_output (str): raw output string of the micro-benchmark.
-
-        Return:
-            True if the raw output string is valid and result can be extracted.
-        """
-        self._result.add_raw_data('raw_output_' + str(cmd_idx), raw_output, self._args.log_raw_data)
-
-        try:
-            for output_line in raw_output.strip().splitlines():
-                self._result.add_result(output_line.strip())
-        except BaseException as e:
-            self._result.set_return_code(ReturnCode.MICROBENCHMARK_RESULT_PARSING_FAILURE)
-            logger.error(
-                'The result format is invalid - round: {}, benchmark: {}, raw output: {}, message: {}.'.format(
-                    self._curr_run_index, self._name, raw_output, str(e)
-                )
-            )
-            return False
-
-        return True
-
-if "x86_64" in platform.machine():
-    BenchmarkRegistry.register_benchmark('cpu-memory-bw-latency', CpuMemBwLatencyBenchmarkMlc)
-else:
-    BenchmarkRegistry.register_benchmark('cpu-memory-bw-latency', CpuMemBwLatencyBenchmarkGeneral)
+BenchmarkRegistry.register_benchmark('cpu-memory-bw-latency', CpuMemBwLatencyBenchmark)
