60eae3610b40a184d66fc71ef32fedd4a88cfefe
dilip patlolla <dilipreddi@gmail.com>
2024-11-18 15:29:57 -0500

    update transformers version for LLamaConfig

--- setup.py
+++ setup.py
@@ -211,7 +211,7 @@ setup(
             'torch': [
                 'torch>=1.7.0a0',
                 'torchvision>=0.8.0a0',
-                'transformers>=4.3.3, <4.23.0',
+                'transformers>=4.28.0',
             ],
             'ort': [
                 'onnx>=1.10.2',
