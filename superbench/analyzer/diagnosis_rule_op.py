4163a28dcc585f3cb1a19fd22bbb193e216817a8
Jorge Esguerra <jorgeesg@microsoft.com>
2025-02-26 15:31:53 -0800

    style: properly format new error message

--- superbench/analyzer/diagnosis_rule_op.py
+++ superbench/analyzer/diagnosis_rule_op.py
@@ -131,9 +131,12 @@ class RuleOp:
                 val = data_row[metric]
                 baseline = rule['metrics'][metric]
                 if baseline is None or baseline == 0:
-                    logger.log_and_raise(
-                        exception=ValueError, msg=f'For metric: {metric}, the baseline value {baseline} is invalid. The node measured value for this metric is: {val}. To debug, review your baseline, summary rule, and diagnosis rule files.'
-                    )
+                    error_msg = (
+                        'For metric: {}, the baseline value {} is invalid for variance calculation. '
+                        'The node measured value for this metric is: {:.4f}. '
+                        'Review your baseline and rule files.'
+                    ).format(metric, baseline, val)
+                    logger.log_and_raise(exception=ValueError, msg=error_msg)
                 var = (val - baseline) / baseline
                 summary_data_row[metric] = var
                 violate_metric = eval(rule['criteria'])(var)
