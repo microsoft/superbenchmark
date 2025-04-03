8558be5909a954b1ab2e6584bf79bf6ca2d8ebdc
Wenqing Lan <wenqinglan@microsoft.com>
2025-04-03 18:08:02 +0000

    add gpu-stream micro bench

---
+++ superbench/benchmarks/micro_benchmarks/gpu_stream.py
@@ -0,0 +1,119 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+"""Module of the GPU Stream Performance benchmark."""
+
+import os
+
+from superbench.common.utils import logger
+from superbench.benchmarks import BenchmarkRegistry, ReturnCode
+from superbench.benchmarks.micro_benchmarks import MicroBenchmarkWithInvoke
+
+
+class GpuStreamBenchmark(MicroBenchmarkWithInvoke):
+    """The GPU stream performance benchmark class."""
+    def __init__(self, name, parameters=''):
+        """Constructor.
+
+        Args:
+            name (str): benchmark name.
+            parameters (str): benchmark parameters.
+        """
+        super().__init__(name, parameters)
+
+        self._bin_name = 'gpu_stream'
+
+    def add_parser_arguments(self):
+        """Add the specified arguments."""
+        super().add_parser_arguments()
+
+        self._parser.add_argument(
+            '--size',
+            type=int,
+            default= 4096 * 1024**2,
+            required=False,
+            help='Size of data buffer in bytes.',
+        )
+
+        self._parser.add_argument(
+            '--num_warm_up',
+            type=int,
+            default=20,
+            required=False,
+            help='Number of warm up rounds',
+        )
+
+        self._parser.add_argument(
+            '--num_loops',
+            type=int,
+            default=100,
+            required=False,
+            help='Number of data buffer copies performed.',
+        )
+
+        self._parser.add_argument(
+            '--check_data',
+            action='store_true',
+            help='Enable data checking',
+        )
+
+    def _preprocess(self):
+        """Preprocess/preparation operations before the benchmarking.
+
+        Return:
+            True if _preprocess() succeed.
+        """
+        if not super()._preprocess():
+            return False
+
+        self.__bin_path = os.path.join(self._args.bin_dir, self._bin_name)
+
+        args = '--size %d --num_warm_up %d --num_loops %d ' % (
+            self._args.size, self._args.num_warm_up, self._args.num_loops
+        )
+
+        if self._args.check_data:
+            args += ' --check_data'
+
+        self._commands = ['%s %s' % (self.__bin_path, args)]
+
+        return True
+
+    def _process_raw_result(self, cmd_idx, raw_output):
+        """Function to parse raw results and save the summarized results.
+
+          self._result.add_raw_data() and self._result.add_result() need to be called to save the results.
+
+        Args:
+            cmd_idx (int): the index of command corresponding with the raw_output.
+            raw_output (str): raw output string of the micro-benchmark.
+
+        Return:
+            True if the raw output string is valid and result can be extracted.
+        """
+        self._result.add_raw_data('raw_output_' + str(cmd_idx), raw_output, self._args.log_raw_data)
+
+        try:
+            output_lines = [x.strip() for x in raw_output.strip().splitlines()]
+            count = 0
+            for output_line in output_lines:
+                if output_line.startswith("STREAM_"):
+                    count += 1
+                    tag, bw_str, pct = output_line.split()
+                    self._result.add_result(tag + '_bw', float(bw_str))
+                    self._result.add_result(tag + '_pct', float(pct))
+            if count == 0:
+                raise BaseException("No valid results found.")
+        except BaseException as e:
+            self._result.set_return_code(ReturnCode.MICROBENCHMARK_RESULT_PARSING_FAILURE)
+            logger.error(
+                'The result format is invalid - round: {}, benchmark: {}, raw output: {}, message: {}.'.format(
+                    self._curr_run_index, self._name, raw_output, str(e)
+                )
+            )
+            return False
+
+        return True
+
+
+BenchmarkRegistry.register_benchmark('gpu-stream', GpuStreamBenchmark)
---
+++ superbench/benchmarks/micro_benchmarks/gpu_stream/gpu_stream_utils.hpp
@@ -0,0 +1,131 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include <array>
+#include <chrono>
+#include <getopt.h>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <numa.h>
+#include <nvml.h>
+
+// Custom deleter for GPU buffers
+struct GpuBufferDeleter {
+    template <typename T> void operator()(T *ptr) const {
+        if (ptr) {
+            cudaFree(ptr);
+        }
+    }
+};
+
+unsigned long long getCurrentTimestampInMicroseconds();
+
+namespace stream_config {
+constexpr std::array<int, 4> kThreadsPerBlock = {128, 256, 512, 1024}; // Threads per block
+constexpr uint64_t kDefaultBufferSizeInBytes = 4294967296;             // Default buffer size 4GB
+constexpr int kNumLoopUnroll = 2;                                      // Unroll depth in SM copy kernel
+constexpr int kNumBuffers = 3;                                         // Number of buffers for triad, add kernel
+constexpr int kNumValidationBuffers = 4;                          // Number of validation buffers, one for each kernel
+constexpr int kUInt8Mod = 256;                                    // Modulo for unsigned long data type
+constexpr std::array<int, 4> kBufferBwMultipliers = {2, 2, 3, 3}; // Buffer multiplier for triad, add kernel
+constexpr long scalar = 11;                                       // Scalar for scale, triad kernel
+
+// Enum for different kernels
+enum class Kernel {
+    kCopy,
+    kScale,
+    kAdd,
+    kTriad,
+    kCount // Add a count to keep track of the number of enums. Helpful for iterating over enums.
+};
+
+// Arguments for each sub benchmark run.
+template <typename T> struct SubBenchArgs {
+    // Unique pointer for GPU buffers
+    using GpuBufferUniquePtr = std::unique_ptr<T, GpuBufferDeleter>;
+
+    // Original data buffer.
+    T *data_buf = nullptr;
+
+    // Buffer to validate the correctness of data transfer.
+    T *check_buf = nullptr;
+
+    // GPU pointer of the data buffer on source devices.
+    std::vector<GpuBufferUniquePtr> gpu_buf_ptrs;
+
+    // Pointer of the validation buffers for each kernel. Order is same as Kernel enum.
+    std::vector<std::vector<T>> validation_buf_ptrs;
+
+    // CUDA stream to be used.
+    cudaStream_t stream;
+
+    // CUDA event to record start time.
+    cudaEvent_t start_event;
+
+    // CUDA event to record end time.
+    cudaEvent_t end_event;
+
+    // CUDA event to record end time.
+    std::vector<std::vector<float>> times_in_ms;
+
+    // Stream Kernel name.
+    std::string kernel_name;
+};
+
+// Arguments for each benchmark run.
+template <typename T> struct BenchArgs {
+
+    // NUMA node under which the benchmark is done.
+    uint64_t numa_id = 0;
+
+    // GPU ID for device.
+    int gpu_id = 0;
+
+    // GPU device info
+    cudaDeviceProp gpu_device_prop;
+
+    // Data buffer size used.
+    uint64_t size = kDefaultBufferSizeInBytes;
+
+    // Number of warm up rounds to run.
+    uint64_t num_warm_up = 0;
+
+    // Number of loops to run.
+    uint64_t num_loops = 1;
+
+    // Whether check data after copy.
+    bool check_data = false;
+
+    // Sub-benchmarks in parallel.
+    SubBenchArgs<T> sub;
+};
+
+// Options accepted by this program.
+struct Opts {
+    // Data buffer size for copy benchmark.
+    uint64_t size = kDefaultBufferSizeInBytes;
+
+    // Number of warm up rounds to run.
+    uint64_t num_warm_up = 0;
+
+    // Number of loops to run.
+    uint64_t num_loops = 0;
+
+    // Whether check data after copy.
+    bool check_data = false;
+};
+
+std::string KernelToString(int); // Function to convert enum to string
+int ParseOpts(int, char **, Opts *);
+void PrintInputInfo(Opts &);
+void PrintUsage();
+
+} // namespace stream_config
