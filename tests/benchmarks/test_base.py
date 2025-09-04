6497bf5bcdeffeff8536e4c278bf80bdcd6a5366
Aishwarya Tonpe <atonpe@microsoft.com>
2025-09-04 18:28:38 +0000

    Adding test for coverage

--- tests/benchmarks/test_base.py
+++ tests/benchmarks/test_base.py
@@ -75,3 +75,27 @@ class BenchmarkBaseTestCase(unittest.TestCase):
                     killer.join()
                 proc.join()
                 self.assertEqual(self.rc_queue.get(block=True, timeout=3), test_case['return_code'])
+
+    def test_compare_log_override(self):
+        """Test argument override from compare_log metadata."""
+
+        class DummyBenchmark(Benchmark):
+            def add_parser_arguments(self):
+                self._parser.add_argument('--compare_log', type=str, required=False)
+                self._parser.add_argument('--foo', type=int, default=1)
+
+            def _benchmark(self):
+                return True
+        # Patch model_log_utils.load_model_log to return dummy metadata
+        from superbench.common import model_log_utils
+        orig_load = model_log_utils.load_model_log
+        model_log_utils.load_model_log = lambda path: {'metadata': {'foo': 42}}
+        try:
+            bench = DummyBenchmark('dummy', parameters='--compare_log dummy_path')
+            bench._benchmark_type = BenchmarkType.MICRO
+            bench.add_parser_arguments()
+            ret, args, unknown = bench.parse_args()
+            assert ret
+            assert args.foo == 42
+        finally:
+            model_log_utils.load_model_log = orig_load
