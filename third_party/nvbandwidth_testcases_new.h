823c4e8ba290cefafed59528d5831d309bd2e1db
454314380 <454314380@qq.com>
2025-09-28 16:29:38 +0800

    clang format and rename testcases_new.h to nvbandwidth_testcases_new.h according to comment

--- third_party/nvbandwidth_testcases_new.h
+++ third_party/nvbandwidth_testcases_new.h
@@ -2,58 +2,62 @@
 // Licensed under the MIT License.
 
 #include "common.h"
+#include "inline_common.h"
 #include "output.h"
 #include "testcase.h"
-#include "inline_common.h"
-
 
 // All to Host Batch CE memcpy using cuMemcpyAsync
-class AllToHostBatchCE: public Testcase {
+class AllToHostBatchCE : public Testcase {
   public:
-     AllToHostBatchCE() : Testcase("all_to_host_batch_memcpy_ce",
-             "\tMeasures bandwidth of cuMemcpyAsync from all devices to host simultaneously.\n"
-             "\tAll devices perform memcpy operations concurrently with the same buffer size.\n"
-             "\tIndividual device bandwidths are measured and reported separately.") {}
-     virtual ~AllToHostBatchCE() {}
-     void run(unsigned long long size, unsigned long long loopCount);
- };
+    AllToHostBatchCE()
+        : Testcase("all_to_host_batch_memcpy_ce",
+                   "\tMeasures bandwidth of cuMemcpyAsync from all devices to host simultaneously.\n"
+                   "\tAll devices perform memcpy operations concurrently with the same buffer size.\n"
+                   "\tIndividual device bandwidths are measured and reported separately.") {}
+    virtual ~AllToHostBatchCE() {}
+    void run(unsigned long long size, unsigned long long loopCount);
+};
 
 // Host to All Batch CE memcpy using cuMemcpyAsync
-class HostToAllBatchCE: public Testcase {
+class HostToAllBatchCE : public Testcase {
   public:
-     HostToAllBatchCE() : Testcase("host_to_all_batch_memcpy_ce",
-             "\tMeasures bandwidth of cuMemcpyAsync from host to all devices simultaneously.\n"
-             "\tAll devices perform memcpy operations concurrently with the same buffer size.\n"
-             "\tIndividual device bandwidths are measured and reported separately.") {}
-     virtual ~HostToAllBatchCE() {}
-     void run(unsigned long long size, unsigned long long loopCount);
- };
+    HostToAllBatchCE()
+        : Testcase("host_to_all_batch_memcpy_ce",
+                   "\tMeasures bandwidth of cuMemcpyAsync from host to all devices simultaneously.\n"
+                   "\tAll devices perform memcpy operations concurrently with the same buffer size.\n"
+                   "\tIndividual device bandwidths are measured and reported separately.") {}
+    virtual ~HostToAllBatchCE() {}
+    void run(unsigned long long size, unsigned long long loopCount);
+};
 
 // All to Host Batch SM memcpy using a copy kernel
-class AllToHostBatchSM: public Testcase {
+class AllToHostBatchSM : public Testcase {
   public:
-     AllToHostBatchSM() : Testcase("all_to_host_batch_memcpy_sm",
-             "\tMeasures bandwidth of copy kernels from all devices to host simultaneously.\n"
-             "\tAll devices perform memcpy operations concurrently with the same buffer size.\n"
-             "\tIndividual device bandwidths are measured and reported separately.") {}
-     virtual ~AllToHostBatchSM() {}
-     void run(unsigned long long size, unsigned long long loopCount);
- };
+    AllToHostBatchSM()
+        : Testcase("all_to_host_batch_memcpy_sm",
+                   "\tMeasures bandwidth of copy kernels from all devices to host simultaneously.\n"
+                   "\tAll devices perform memcpy operations concurrently with the same buffer size.\n"
+                   "\tIndividual device bandwidths are measured and reported separately.") {}
+    virtual ~AllToHostBatchSM() {}
+    void run(unsigned long long size, unsigned long long loopCount);
+};
 
 // Host to All Batch SM memcpy using a copy kernel
-class HostToAllBatchSM: public Testcase {
+class HostToAllBatchSM : public Testcase {
   public:
-     HostToAllBatchSM() : Testcase("host_to_all_batch_memcpy_sm",
-             "\tMeasures bandwidth of copy kernels from host to all devices simultaneously.\n"
-             "\tAll devices perform memcpy operations concurrently with the same buffer size.\n"
-             "\tIndividual device bandwidths are measured and reported separately.") {}
-     virtual ~HostToAllBatchSM() {}
-     void run(unsigned long long size, unsigned long long loopCount);
- };
+    HostToAllBatchSM()
+        : Testcase("host_to_all_batch_memcpy_sm",
+                   "\tMeasures bandwidth of copy kernels from host to all devices simultaneously.\n"
+                   "\tAll devices perform memcpy operations concurrently with the same buffer size.\n"
+                   "\tIndividual device bandwidths are measured and reported separately.") {}
+    virtual ~HostToAllBatchSM() {}
+    void run(unsigned long long size, unsigned long long loopCount);
+};
 
-void Testcase::allHostHelperBatch(unsigned long long size, MemcpyOperation &memcpyInstance, PeerValueMatrix<double> &bandwidthValues, bool sourceIsHost) {
-    std::vector<const MemcpyBuffer*> allSrcBuffers;
-    std::vector<const MemcpyBuffer*> allDstBuffers;
+void Testcase::allHostHelperBatch(unsigned long long size, MemcpyOperation &memcpyInstance,
+                                  PeerValueMatrix<double> &bandwidthValues, bool sourceIsHost) {
+    std::vector<const MemcpyBuffer *> allSrcBuffers;
+    std::vector<const MemcpyBuffer *> allDstBuffers;
 
     // Create buffers for all devices with the same size
     for (int deviceId = 0; deviceId < deviceCount; deviceId++) {
