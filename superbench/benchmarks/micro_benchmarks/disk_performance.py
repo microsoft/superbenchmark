e41fe3fc91b58ca93263737aed638d2cbef15633
Ziyue Yang <ziyyang@microsoft.com>
2025-09-29 10:43:01 +0000

    fix lint

--- superbench/benchmarks/micro_benchmarks/disk_performance.py
+++ superbench/benchmarks/micro_benchmarks/disk_performance.py
@@ -130,8 +130,10 @@ class DiskBenchmark(MicroBenchmarkWithInvoke):
             '--verify',
             type=str,
             required=False,
-            help=
-            'Verification method specified for fio --verify flag. See https://fio.readthedocs.io/en/latest/fio_doc.html#cmdoption-arg-verify.',
+            help=(
+                'Verification method specified for fio --verify flag. '
+                'See https://fio.readthedocs.io/en/latest/fio_doc.html#cmdoption-arg-verify.',
+            ),
         )
 
     def _get_arguments_from_env(self):
