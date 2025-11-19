5d4f6d9aa8881cfa9f4727cb933905856a09df5b
AVA <39534996+avazr@users.noreply.github.com>
2025-11-19 17:59:41 -0600

    Lint fix

--- superbench/runner/runner.py
+++ superbench/runner/runner.py
@@ -154,8 +154,8 @@ class SuperBenchRunner():
         elif mode.name == 'torch.distributed':
             torch_dist_params = (
                 f'--nnodes={mode.node_num} --rdzv-endpoint=$MASTER_ADDR:$MASTER_PORT '
-                f'--rdzv-id={random.randint(100, 999)} --rdzv-backend=c10d '
-                if ('node_num' in mode and str(mode.node_num).isdigit() and int(mode.node_num) > 1) else ''
+                f'--rdzv-id={random.randint(100, 999)} --rdzv-backend=c10d ' if
+                ('node_num' in mode and str(mode.node_num).isdigit() and int(mode.node_num) > 1) else ''
             )
 
             nsys_prefix = (
