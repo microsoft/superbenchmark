ee52086b8a8bcad0a56dc2b354e9ba2fc5f4cde0
Wenqing Lan <wenqinglan@microsoft.com>
2026-05-21 12:07:45 -0700

    fix lint

--- tests/benchmarks/micro_benchmarks/test_gpu_stream.py
+++ tests/benchmarks/micro_benchmarks/test_gpu_stream.py
@@ -107,7 +107,9 @@ class GpuStreamBenchmarkTest(BenchmarkTestCase, unittest.TestCase):
                     assert (test_raw_output_dict[output_key.removesuffix('_bw')][0] == benchmark.result[output_key][0])
                 else:
                     assert (output_key.removesuffix('_ratio') in test_raw_output_dict)
-                    assert (test_raw_output_dict[output_key.removesuffix('_ratio')][1] == benchmark.result[output_key][0])
+                    assert (
+                        test_raw_output_dict[output_key.removesuffix('_ratio')][1] == benchmark.result[output_key][0]
+                    )
 
         # Negative case - invalid raw output.
         assert (benchmark._process_raw_result(1, 'Invalid raw output') is False)
@@ -152,7 +154,9 @@ class GpuStreamBenchmarkTest(BenchmarkTestCase, unittest.TestCase):
                     assert (test_raw_output_dict[output_key.removesuffix('_bw')][0] == benchmark.result[output_key][0])
                 else:
                     assert (output_key.removesuffix('_ratio') in test_raw_output_dict)
-                    assert (test_raw_output_dict[output_key.removesuffix('_ratio')][1] == benchmark.result[output_key][0])
+                    assert (
+                        test_raw_output_dict[output_key.removesuffix('_ratio')][1] == benchmark.result[output_key][0]
+                    )
 
     @decorator.cuda_test
     def test_gpu_stream_result_parsing_cuda_float(self):
